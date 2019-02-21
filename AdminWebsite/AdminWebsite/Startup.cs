342a3730f7d6af784e764491b5a0ff343c58d982
Lionel Roche <lionel.roche@hmcts.net>
2019-02-21 12:00:08 +0000

    Removed Infrastructure folder, removed commented code, added launchSettings.

--- AdminWebsite/AdminWebsite/Startup.cs
+++ AdminWebsite/AdminWebsite/Startup.cs
@@ -88,7 +88,6 @@ namespace AdminWebsite
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                // app.UseHsts();
             }
             
             app.UseHttpsRedirection();
