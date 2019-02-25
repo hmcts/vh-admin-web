d6a9e4571b42b77fdc84a338088237c143cbe881
Travis CI <travis@travis-ci.org>
2019-02-25 09:19:35 +0000

    other information page-2641

--- AdminWebsite/AdminWebsite/ClientApp/src/app/booking/other-information/other-information.component.spec.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/booking/other-information/other-information.component.spec.ts
@@ -1,36 +1,71 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { AbstractControl } from '@angular/forms';
 import { BreadcrumbComponent } from '../breadcrumb/breadcrumb.component';
 import { OtherInformationComponent } from './other-information.component';
+import { VideoHearingsService } from '../../services/video-hearings.service';
+import { CancelPopupStubComponent } from 'src/app/testing/stubs/cancel-popup-stub';
+import { ConfirmationPopupStubComponent } from 'src/app/testing/stubs/confirmation-popup-stub';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 let routerSpy: jasmine.SpyObj<Router>;
+let otherInformation: AbstractControl;
+let videoHearingsServiceSpy: jasmine.SpyObj<VideoHearingsService>;
 
 describe('OtherInformationComponent', () => {
   let component: OtherInformationComponent;
   let fixture: ComponentFixture<OtherInformationComponent>;
+  videoHearingsServiceSpy = jasmine.createSpyObj<VideoHearingsService>('VideoHearingsService',
+    ['getOtherInformation', 'setOtherInformation', 'removeOtherInformation', 'cancelRequest']);
 
   beforeEach(async(() => {
     routerSpy = jasmine.createSpyObj('Router', ['navigate']);
 
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, SharedModule],
       providers: [
-        { provide: Router, useValue: routerSpy }
+        { provide: Router, useValue: routerSpy },
+        { provide: VideoHearingsService, useValue: videoHearingsServiceSpy }
       ],
-      declarations: [ OtherInformationComponent, BreadcrumbComponent ]
+      declarations: [
+        OtherInformationComponent,
+        BreadcrumbComponent,
+        CancelPopupStubComponent,
+        ConfirmationPopupStubComponent
+      ]
     })
-    .compileComponents();
+      .compileComponents();
+    videoHearingsServiceSpy.getOtherInformation.and.returnValue('some text');
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(OtherInformationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    otherInformation = component.otherInformationForm.controls['otherInformation'];
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should set initiall values for field', () => {
+    component.ngOnInit();
+    expect(otherInformation.value).toBe('some text');
+    expect(component.otherInformationText).toBe('some text');
+  });
+  it('if press next button should save other information in storage and navigat to summary page.', () => {
+    component.ngOnInit();
+    component.next();
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/summary']);
+    const param = 'some text';
+    expect(videoHearingsServiceSpy.setOtherInformation).toHaveBeenCalledWith(param);
+  });
+  it('if press cancel button should remove other information from storage and navigat to dushboard page.', () => {
+    component.ngOnInit();
+    component.otherInformationCancel();
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/dashboard']);
+    expect(videoHearingsServiceSpy.removeOtherInformation).toHaveBeenCalled();
+    expect(videoHearingsServiceSpy.cancelRequest).toHaveBeenCalled();
   });
 });
