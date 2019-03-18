867f5695fac566c03d561729b72c72dfaf167085
Lionel Roche <lionel.roche@hmcts.net>
2019-03-18 10:35:31 +0000

    fixes for SonarCloud Code Analysis

--- AdminWebsite/AdminWebsite/Security/UserApiTokenHandler.cs
+++ AdminWebsite/AdminWebsite/Security/UserApiTokenHandler.cs
@@ -4,8 +4,18 @@ using Microsoft.Extensions.Options;
 
 namespace AdminWebsite.Security
 {
+    /// <summary>
+    ///     The User API Token handler class.
+    /// </summary>
     public class UserApiTokenHandler : BaseServiceTokenHandler
     {
+        /// <summary>
+        ///     User API token handler.
+        /// </summary>
+        /// <param name="securitySettings"></param>
+        /// <param name="serviceSettings"></param>
+        /// <param name="memoryCache"></param>
+        /// <param name="tokenProvider"></param>
         public UserApiTokenHandler(IOptions<SecuritySettings> securitySettings, 
             IOptions<ServiceSettings> serviceSettings, IMemoryCache memoryCache, 
             ITokenProvider tokenProvider)
@@ -13,7 +23,14 @@ namespace AdminWebsite.Security
         {
         }
 
+        /// <summary>
+        ///     The User API token cache.
+        /// </summary>
         protected override string TokenCacheKey => "UserApiServiceToken";
+
+        /// <summary>
+        ///     The User API resource Id.
+        /// </summary>
         protected override string ClientResource => ServiceSettings.UserApiResourceId;
     }
 }
