3167678a608e84c31188c0c58f3e59395dafd94a
Travis CI <travis@travis-ci.org>
2019-03-28 14:47:37 +0000

    changes in api client -3734

--- AdminWebsite/AdminWebsite/ClientApp/src/app/services/video-hearings.service.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/services/video-hearings.service.ts
@@ -3,7 +3,10 @@ import { Observable, of } from 'rxjs';
 import {
   HearingTypeResponse, BHClient, BookNewHearingRequest, HearingDetailsResponse,
   CaseAndHearingRolesResponse, CaseRequest, ParticipantRequest, CaseResponse2,
-  ParticipantResponse
+  ParticipantResponse,
+  EditHearingRequest,
+  EditCaseRequest,
+  EditParticipantRequest
 } from './clients/api-client';
 import { HearingModel } from '../common/model/hearing.model';
 import { CaseModel } from '../common/model/case.model';
@@ -85,15 +88,48 @@ export class VideoHearingsService {
 
   updateHearing(booking: HearingModel): Observable<HearingDetailsResponse> {
     const hearingRequest = this.mapExistingHearing(booking);
-    // to do waiting for api
-    return of(hearingRequest);
+    return this.bhClient.editHearing(booking.hearing_id, hearingRequest);
   }
 
-  mapExistingHearing(booking: HearingModel): HearingDetailsResponse {
-    return new HearingDetailsResponse();
+  mapExistingHearing(booking: HearingModel): EditHearingRequest {
+    const hearing = new EditHearingRequest();
+    if (booking.cases && booking.cases.length > 0) {
+      hearing.case = new EditCaseRequest({ name: booking.cases[0].name, number: booking.cases[0].number });
+    }
+    hearing.hearing_room_name = booking.court_room;
+    hearing.hearing_venue_name = booking.court_name;
+    hearing.other_information = booking.other_information;
+    hearing.scheduled_date_time = booking.scheduled_date_time;
+    hearing.scheduled_duration = booking.scheduled_duration;
+    hearing.participants = this.mapParticipantModelToEditParticipantRequest(booking.participants);
+    return hearing;
   }
 
-  mapHearing(newRequest: HearingModel): HearingDetailsResponse {
+  mapParticipantModelToEditParticipantRequest(participants: ParticipantModel[]): EditParticipantRequest[] {
+    let list: Array<EditParticipantRequest> = [];
+    if (participants && participants.length > 0) {
+      list = participants.map(x => this.mappingToEditParticipantRequest(x));
+      }
+    return list;
+  }
+
+  mappingToEditParticipantRequest(participant: ParticipantModel): EditParticipantRequest {
+    const editParticipant = new EditParticipantRequest();
+    editParticipant.case_role_name = participant.case_role_name;
+    editParticipant.contact_email = participant.email;
+    editParticipant.display_name = participant.display_name;
+    editParticipant.first_name = participant.first_name;
+    editParticipant.last_name = participant.last_name;
+    editParticipant.hearing_role_name = participant.hearing_role_name;
+    editParticipant.middle_names = participant.middle_names;
+    editParticipant.representee = participant.representee;
+    editParticipant.solicitors_reference = participant.solicitorsReference;
+    editParticipant.telephone_number = participant.phone;
+    editParticipant.title = participant.title;
+    return editParticipant;
+  }
+
+  mapHearing(newRequest: HearingModel): BookNewHearingRequest {
     const newHearingRequest = new BookNewHearingRequest();
     newHearingRequest.cases = this.mapCases(newRequest);
     newHearingRequest.case_type_name = newRequest.case_type;
