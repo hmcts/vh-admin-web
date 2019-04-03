fc6dd662b4b6f4955348ae6baa054573ac2fd7b9
David Dikman <ddikman@gmail.com>
2019-04-03 17:47:12 +0100

    Added additional test for getting of the judges.

--- AdminWebsite/AdminWebsite.IntegrationTests/Controllers/UserDataController/GetJudgesTests.cs
+++ AdminWebsite/AdminWebsite.IntegrationTests/Controllers/UserDataController/GetJudgesTests.cs
@@ -1,9 +1,11 @@
+using System;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using AdminWebsite.Contracts.Responses;
 using FluentAssertions;
-using Newtonsoft.Json;
 using NUnit.Framework;
+using Testing.Common;
 
 namespace AdminWebsite.IntegrationTests.Controllers.UserDataController
 {
@@ -15,8 +17,21 @@ namespace AdminWebsite.IntegrationTests.Controllers.UserDataController
             var responseMessage = await SendGetRequestAsync("/api/accounts/judges");
             responseMessage.StatusCode.Should().Be(HttpStatusCode.OK);
             var resultJson = await responseMessage.Content.ReadAsStringAsync();
-            var judges = JsonConvert.DeserializeObject<ParticipantDetailsResponse[]>(resultJson);
-            judges.Length.Should().BeGreaterThan(0);
+            var judges = ApiRequestHelper.DeserialiseSnakeCaseJsonToResponse<ParticipantDetailsResponse[]>(resultJson);
+
+            var testJudge = judges.Single(j =>
+                j.Email.Equals("Test.Judge01@hearings.reform.hmcts.net", StringComparison.CurrentCultureIgnoreCase));
+
+            Guid.TryParse(testJudge.Id, out _).Should().BeTrue();
+            testJudge.LastName.Should().Be("Judge01");
+            testJudge.FirstName.Should().Be("Test");
+            testJudge.DisplayName.Should().Be("Test Judge01");
+            
+            // These aren't part of the active directory data, really, they should not be a part of the result
+            testJudge.MiddleName.Should().Be("");
+            testJudge.Role.Should().BeNull();
+            testJudge.Title.Should().BeNull();
+            testJudge.Phone.Should().BeNull();
         }
     }
 }
