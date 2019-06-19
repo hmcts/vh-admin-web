11f143c4b2ade39643d6aa174ea0236bf9eebe13
David <ddikman@gmail.com>
2019-06-19 17:17:58 +0100

    uglycode for testing the scrolling in different browser

--- AdminWebsite/AdminWebsite/ClientApp/src/app/shared/directives/scrollable.directive.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/shared/directives/scrollable.directive.ts
@@ -1,8 +1,9 @@
-import { Directive, HostListener, EventEmitter, Output, ElementRef, Inject, AfterViewInit } from '@angular/core';
+import { Directive, HostListener, EventEmitter, Output, ElementRef, Inject, AfterViewInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/pairwise';
 import 'rxjs/add/operator/map';
+import { DOCUMENT } from '@angular/platform-browser';
 
 interface ScrollPosition {
   pageYOffset: number;
@@ -14,29 +15,56 @@ interface ScrollPosition {
 @Directive({
   selector: '[appScrollable]'
 })
-export class ScrollableDirective implements AfterViewInit {
+export class ScrollableDirective implements AfterViewInit, OnDestroy {
 
   private $scrollEvent: EventEmitter<any> = new EventEmitter();
   private $userScrolledDown: Observable<any>;
   private $requestCallBack: Observable<any>;
+  private rootElement: ElementRef;
 
   lastPosition = 0;
 
   @Output() scrollPosition = new EventEmitter();
 
-  constructor(public el: ElementRef) { }
+  constructor(@Inject(DOCUMENT) private document: Document, private element: ElementRef ) {
+  }
 
   ngAfterViewInit() {
-    this.streamScrollEvents();
-    this.requestCallbackOnScroll();
+    //this.streamScrollEvents();
+    //this.requestCallbackOnScroll();
+  }
+
+  ngOnDestroy() {
+  }
+
+  private pastBottom = false;
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    const offset = window.pageYOffset || this.document.documentElement.scrollTop || this.document.body.scrollTop || 0;
+    const scrollBottom = offset + this.document.documentElement.clientHeight;
+    const clientBottom = this.element.nativeElement.clientHeight + this.element.nativeElement.offsetTop;
+    if (scrollBottom > clientBottom) {
+      // raise event only if we haven't already past bottom
+      if (!this.pastBottom) {
+        console.log('past bottom');
+        this.scrollPosition.emit();
+        this.pastBottom = true;
+      }
+    } else if (this.pastBottom && scrollBottom < clientBottom) {
+      if (this.pastBottom) {
+        console.log('scroll up');
+        this.pastBottom = false;
+      }
+    }
   }
 
   private streamScrollEvents() {
     this.$userScrolledDown = this.$scrollEvent.asObservable()
       .map((e: any): ScrollPosition => ({
         pageYOffset: e.target.scrollTop,
-        innerHeight: this.el.nativeElement.scrollHeight,
-        offsetHeight: this.el.nativeElement.offsetHeight,
+        innerHeight: this.rootElement.nativeElement.scrollHeight,
+        offsetHeight: this.rootElement.nativeElement.offsetHeight,
       }))
       .pairwise()
       .filter(positions => this.isUserScrollingDown(positions) && this.isLoadingScrollPosition(positions[1]));
