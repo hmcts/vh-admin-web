ee1fb06aa15af49c76b91ab1c8a4dafdfeaf66c2
kkodalikk <karuna.kodali@hmcts.net>
2019-08-01 12:08:45 +0100

    IE11 Banner functionality

--- AdminWebsite/AdminWebsite/ClientApp/src/app/app-routing.module.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/app-routing.module.ts
@@ -7,23 +7,25 @@ import { LogoutComponent } from './security/logout.component';
 import { UnauthorisedComponent } from './error/unauthorised.component';
 import { ErrorComponent } from './error/error.component';
 import { AdminGuard } from './security/admin.guard';
+import { UnsupportedBrowserComponent } from './shared/unsupported-browser/unsupported-browser.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-    { path: 'dashboard', component: DashboardComponent, canActivate: [AdminGuard] },
-    { path: 'login', component: LoginComponent },
-    { path: 'logout', component: LogoutComponent },
-    { path: 'unauthorised', component: UnauthorisedComponent },
-    { path: 'error', component: ErrorComponent },
-    { path: '**', redirectTo: 'dashboard', pathMatch: 'full', canActivate: [AdminGuard] }
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AdminGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: 'unauthorised', component: UnauthorisedComponent },
+  { path: 'error', component: ErrorComponent },
+  { path: 'unsupported-browser', component: UnsupportedBrowserComponent },
+  { path: '**', redirectTo: 'dashboard', pathMatch: 'full', canActivate: [AdminGuard] }
 ];
 
 @NgModule({
-    exports: [
-        RouterModule
-    ],
-    imports: [
-        RouterModule.forRoot(routes)],
+  exports: [
+    RouterModule
+  ],
+  imports: [
+    RouterModule.forRoot(routes)],
 })
 
 export class AppRoutingModule { }
---
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/shared/unsupported-browser/unsupported-browser.component.spec.ts
@@ -0,0 +1,34 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UnsupportedBrowserComponent } from './unsupported-browser.component';
+import { DeviceType } from 'src/app/services/device-type';
+
+describe('UnsupportedBrowserComponent', () => {
+  let component: UnsupportedBrowserComponent;
+  let fixture: ComponentFixture<UnsupportedBrowserComponent>;
+  let deviceTypeServiceSpy: jasmine.SpyObj<DeviceType>;
+  const browserName = 'Opera';
+
+  beforeEach(async(() => {
+    deviceTypeServiceSpy = jasmine.createSpyObj<DeviceType>(['getBrowserName']);
+    deviceTypeServiceSpy.getBrowserName.and.returnValue(browserName);
+    TestBed.configureTestingModule({
+      declarations: [ UnsupportedBrowserComponent ],
+      providers: [
+        { provide: DeviceType, useValue: deviceTypeServiceSpy }
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UnsupportedBrowserComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should initalise with browser information', () => {
+    expect(component.supportedBrowsers.length).toBeGreaterThan(0);
+    expect(component.browserName).toBe(browserName);
+  });
+});
