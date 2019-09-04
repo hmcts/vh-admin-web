171228598ac7cddb45e993414460bec682a1a4e1
kkodalikk <karuna.kodali@hmcts.net>
2019-09-04 11:41:30 +0100

    Updated Unsupported browser message on Admin-Web

--- AdminWebsite/AdminWebsite/ClientApp/src/app/services/device-type.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/services/device-type.ts
@@ -22,7 +22,7 @@ export class DeviceType {
   }
 
   isSupportedBrowser(): boolean {
-    const supportedBrowsers = ['Firefox', 'Safari', 'Chrome', 'Edge', 'MS-Edge'];
+    const supportedBrowsers = ['Chrome'];
     const browser = this.deviceDetectorService.browser;
     return supportedBrowsers.findIndex(x => x.toUpperCase() === browser.toUpperCase()) > -1;
   }
