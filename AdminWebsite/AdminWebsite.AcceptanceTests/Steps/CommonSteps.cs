1fb056fbd3b31c1b271f31bfbef0004b0d760302
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2019-12-19 16:24:44 +0100

    Disabled an acceessibility warning

--- AdminWebsite/AdminWebsite.AcceptanceTests/Steps/CommonSteps.cs
+++ AdminWebsite/AdminWebsite.AcceptanceTests/Steps/CommonSteps.cs
@@ -49,6 +49,7 @@ namespace AdminWebsite.AcceptanceTests.Steps
             var axeResult = new AxeBuilder(_browsers[_c.CurrentUser.Key].Driver)
                 .DisableRules( // BUG: Once VIH-5174 bug is fixed, remove these exclusions
                     "region", // https://dequeuniversity.com/rules/axe/3.3/region?application=axeAPI
+                    "landmark-main-is-top-level", // https://dequeuniversity.com/rules/axe/3.3/landmark-main-is-top-level?application=axeAPI
                     "landmark-one-main", // https://dequeuniversity.com/rules/axe/3.3/landmark-one-main?application=axeAPI
                     "landmark-no-duplicate-banner", // https://dequeuniversity.com/rules/axe/3.3/landmark-no-duplicate-banner?application=axeAPI
                     "landmark-no-duplicate-contentinfo", // https://dequeuniversity.com/rules/axe/3.3/landmark-no-duplicate-contentinfo?application=axeAPI
