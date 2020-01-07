09f70621a7850e1f5a76bdb16d491d533fa113a1
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-01-07 13:02:53 +0100

    Added a fix for date format issues

--- AdminWebsite/AdminWebsite.AcceptanceTests/Data/DateFormats.cs
+++ AdminWebsite/AdminWebsite.AcceptanceTests/Data/DateFormats.cs
@@ -4,15 +4,29 @@ namespace AdminWebsite.AcceptanceTests.Data
 {
     public static class DateFormats
     {
-        private static readonly string ChromeHearingDateFormat = System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern;
+        private static string _chromeHearingDateFormat = System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern;
         private const string FirefoxHearingDateFormat = "yyyy-MM-dd";
         public static string HearingSummaryDate = "dddd dd MMMM yyyy, h:mmtt";
 
-        public static string FormatDateToLocalDateFormat(TargetBrowser browser, bool runningWithSauceLabs)
+        public static string FormatDateToLocalDateFormat(TargetBrowser browser)
         {
-            if (browser == TargetBrowser.Chrome)
-                return ChromeHearingDateFormat;
-            return browser == TargetBrowser.Firefox ? FirefoxHearingDateFormat : ChromeHearingDateFormat;
+            if (browser != TargetBrowser.Chrome)
+                return browser == TargetBrowser.Firefox ? FirefoxHearingDateFormat : _chromeHearingDateFormat;
+            AddZerosToShortDateFormat();
+            return _chromeHearingDateFormat;
+        }
+
+        private static void AddZerosToShortDateFormat()
+        {
+            var splitDate = _chromeHearingDateFormat.Split("/");
+            foreach (var portion in splitDate)
+            {
+                if (portion.Equals("d"))
+                    _chromeHearingDateFormat = _chromeHearingDateFormat.Replace("d", "dd");
+
+                if (portion.Equals("M"))
+                    _chromeHearingDateFormat = _chromeHearingDateFormat.Replace("M", "MM");
+            }
         }
     }
 }
