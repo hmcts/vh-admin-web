43b251ac3a28755f25780d9fd6bc56de53ac4ce0
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-01-10 14:42:26 +0100

    Added missing NSwag client for Video Api

--- AdminWebsite/AdminWebsite.AcceptanceTests/Hooks/DriverHooks.cs
+++ AdminWebsite/AdminWebsite.AcceptanceTests/Hooks/DriverHooks.cs
@@ -28,7 +28,7 @@ namespace AdminWebsite.AcceptanceTests.Hooks
         }
 
         [BeforeScenario(Order = (int)HooksSequence.ConfigureDriverHooks)]
-        private void ConfigureDriver(TestContext context, ScenarioContext scenarioContext)
+        public void ConfigureDriver(TestContext context, ScenarioContext scenarioContext)
         {
             context.AdminWebConfig.TestConfig.TargetBrowser = _driverManager.GetTargetBrowser(NUnit.Framework.TestContext.Parameters["TargetBrowser"]);
             context.AdminWebConfig.TestConfig.TargetDevice = _driverManager.GetTargetDevice(NUnit.Framework.TestContext.Parameters["TargetDevice"]);
