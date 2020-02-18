af0685b677c080389fc547ded1e4577e42fec1cf
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-02-18 14:44:22 +0100

    Changed to UI culture

--- AdminWebsite/AdminWebsite.AcceptanceTests/Data/DateFormats.cs
+++ AdminWebsite/AdminWebsite.AcceptanceTests/Data/DateFormats.cs
@@ -4,7 +4,7 @@ namespace AdminWebsite.AcceptanceTests.Data
 {
     public static class DateFormats
     {
-        public static string LocalDateFormat = CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern;
+        public static string LocalDateFormat = CultureInfo.CurrentUICulture.DateTimeFormat.ShortDatePattern;
         public static string HearingSummaryDate = "dddd dd MMMM yyyy, h:mmtt";
     }
 }
