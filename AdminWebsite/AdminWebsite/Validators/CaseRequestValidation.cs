0a7f1dba8747ac68ec81fd2fe0f2450d8f32fdcb
Valentina <Valentina.bennett@hmcts.net>
2020-02-27 15:48:31 +0000

    Fix validation msg for case name

--- AdminWebsite/AdminWebsite/Validators/CaseRequestValidation.cs
+++ AdminWebsite/AdminWebsite/Validators/CaseRequestValidation.cs
@@ -5,18 +5,20 @@ namespace AdminWebsite.Validators
 {
     public class CaseRequestValidation : AbstractValidator<CaseRequest>
     {
-        private const string MESSAGE = "Case number is required between 1 - 255 characters";
+        private const string CASE_NUMBER_MESSAGE = "Case number is required between 1 - 255 characters";
+        private const string CASE_NAME_MESSAGE = "Case name is required between 1 - 255 characters";
+
         public CaseRequestValidation()
         {
             RuleFor(x => x.Number)
                 .NotEmpty()
                 .MaximumLength(255)
-                .WithMessage(MESSAGE);
+                .WithMessage(CASE_NUMBER_MESSAGE);
 
             RuleFor(x => x.Name)
                 .NotEmpty()
                 .MaximumLength(255)
-                .WithMessage(MESSAGE);
+                .WithMessage(CASE_NAME_MESSAGE);
         }
     }
 }
