a0ccf4d131fde2cdcd70e73a7a5b10e188bf917f
Valentina <Valentina.bennett@hmcts.net>
2020-02-28 10:29:02 +0000

    Add unit test

---
+++ AdminWebsite/AdminWebsite.UnitTests/Security/UserServiceExceptionTest.cs
@@ -0,0 +1,49 @@
+﻿using AdminWebsite.BookingsAPI.Client;
+using AdminWebsite.Helper;
+using AdminWebsite.Security;
+using AdminWebsite.Services.Models;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Claims;
+
+namespace AdminWebsite.UnitTests.Security
+{
+    public class UserServiceExceptionTest
+    {
+        private UserServiceException _exception;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _exception = new UserServiceException("Error message", "reason");
+        }
+
+        [Test]
+        public void Should_get_object_data()
+        {
+            var info = new SerializationInfo(typeof(CaseRequest), new FormatterConverter());
+            _exception.GetObjectData(info, new StreamingContext());
+
+            Assert.That(info.ObjectType == typeof(CaseRequest));
+        }
+
+        [Test]
+        public void Should_throw_inner_exception_by_get_object_data_if_null()
+        {
+            try
+            {
+                _exception.GetObjectData(null, new StreamingContext());
+            }
+            catch (ArgumentNullException x)
+            {
+                Assert.That(x.ToString().Contains("Value cannot be null"));
+            }
+        }
+            
+
+    }
+}
