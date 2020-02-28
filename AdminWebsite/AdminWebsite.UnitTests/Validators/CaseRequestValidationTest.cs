c62c85aa4efccdf677cddd7ccc51fe8576c99b64
Valentina <Valentina.bennett@hmcts.net>
2020-02-28 09:11:19 +0000

    Add unit test

---
+++ AdminWebsite/AdminWebsite.UnitTests/Validators/CaseRequestValidationTest.cs
@@ -0,0 +1,55 @@
+﻿using AdminWebsite.BookingsAPI.Client;
+using AdminWebsite.Validators;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace AdminWebsite.UnitTests.Validators
+{
+    public class CaseRequestValidationTest
+    {
+        private CaseRequestValidation _validator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _validator = new CaseRequestValidation();
+        }
+
+        [Test]
+        public void Should_validate_case_number_and_name_with_length_greater_then_255_as_error()
+        {
+        const string CASE_NUMBER_MESSAGE = "Case number is required between 1 - 255 characters";
+         const string CASE_NAME_MESSAGE = "Case name is required between 1 - 255 characters";
+
+        var longString = new String('a', 257);
+            var testRequest = new CaseRequest { Number = longString, Name=longString };
+            var result = _validator.Validate(testRequest);
+            Assert.That(result.Errors.Any(o => o.PropertyName == "Number" && o.ErrorMessage == CASE_NUMBER_MESSAGE));
+            Assert.That(result.Errors.Any(o => o.PropertyName == "Name" && o.ErrorMessage == CASE_NAME_MESSAGE));
+        }
+
+        [Test]
+        public void Should_validate_case_number_and_name_with_length_zero_as_error()
+        {
+            var shortString = "";
+            var testRequest = new CaseRequest { Number = shortString, Name = shortString };
+            var result = _validator.Validate(testRequest);
+            Assert.That(result.Errors.All(o => o.ErrorMessage.Contains("must not be empty.")));
+            Assert.That(result.Errors.Count == 2);
+        }
+
+        [Test]
+        public void Should_validate_case_request()
+        {
+            var testRequest = new CaseRequest
+            {
+                Name = "case name",
+                Number = "case number 1"
+            };
+
+            var result = _validator.Validate(testRequest);
+            Assert.That(result.Errors.Count == 0);
+        }
+    }
+}
---
+++ AdminWebsite/AdminWebsite.UnitTests/Validators/ParticipantRequestValidationTest.cs
@@ -0,0 +1,76 @@
+﻿using AdminWebsite.BookingsAPI.Client;
+using AdminWebsite.Validators;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace AdminWebsite.UnitTests.Validators
+{
+    public class ParticipantRequestValidationTest
+    {
+        private ParticipantRequestValidation _validator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _validator = new ParticipantRequestValidation();
+        }
+
+        [Test]
+        public void Should_validate_fields_with_length_greater_then_255_as_error()
+        {
+            const string EMAIL_MSG = "Email is required in the correct format and between 1 - 255 characters";
+            const string DISPLAY_NAME_MSG = "Display name is required and between 1 - 255 characters";
+            const string FIRST_NAME_MSG = "First name is required and between 1 - 255 characters";
+            const string LASTNAME_MSG = "Lastname is required and between 1 - 255 characters";
+
+
+            var longString = new String('a', 257);
+            var testRequest = new BookingsAPI.Client.ParticipantRequest
+            {
+                Contact_email = longString,
+                Display_name = longString,
+                First_name = longString,
+                Last_name = longString
+            };
+
+            var result = _validator.Validate(testRequest);
+            Assert.That(result.Errors.Any(o => o.PropertyName == "Contact_email" && o.ErrorMessage == EMAIL_MSG));
+            Assert.That(result.Errors.Any(o => o.PropertyName == "Display_name" && o.ErrorMessage == DISPLAY_NAME_MSG));
+            Assert.That(result.Errors.Any(o => o.PropertyName == "First_name" && o.ErrorMessage == FIRST_NAME_MSG));
+            Assert.That(result.Errors.Any(o => o.PropertyName == "Last_name" && o.ErrorMessage == LASTNAME_MSG));
+
+        }
+
+        [Test]
+        public void Should_validate_fields_with_length_zero_as_error()
+        {
+            var shortString = "";
+            var testRequest = new BookingsAPI.Client.ParticipantRequest
+            {
+                Contact_email = shortString,
+                Display_name = shortString,
+                First_name = shortString,
+                Last_name = shortString
+            };
+            var result = _validator.Validate(testRequest);
+            Assert.That(result.Errors.Any(o => o.ErrorMessage.Contains("must not be empty.")));
+            Assert.That(result.Errors.Count == 5);
+        }
+
+        [Test]
+        public void Should_validate_participant_request()
+        {
+            var testRequest = new BookingsAPI.Client.ParticipantRequest
+            {
+                Contact_email = "aa@aa.aa",
+                First_name = "Adam",
+                Last_name = "Adams",
+                Display_name = "Adam"
+            };
+
+            var result = _validator.Validate(testRequest);
+            Assert.That(result.Errors.Count == 0);
+        }
+    }
+}
