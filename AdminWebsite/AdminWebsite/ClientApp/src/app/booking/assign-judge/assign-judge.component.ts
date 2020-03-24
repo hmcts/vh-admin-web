125945e9ca1795fe393d73337aa54d0a88b23024
Lionel Roche <lionel.roche@hmcts.net>
2020-03-24 09:46:34 +0000

    feature/VIH-5703 : Admin_Web_-_Redirecting_to_error_page_when_clicking_next_on_Hearing_Schedule - added check for judgeName

--- AdminWebsite/AdminWebsite/ClientApp/src/app/booking/assign-judge/assign-judge.component.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/booking/assign-judge/assign-judge.component.ts
@@ -112,15 +112,19 @@ export class AssignJudgeComponent extends BookingBaseComponent implements OnInit
       judgeDisplayName: this.judgeDisplayName
     });
 
-    this.$subscriptions.push(this.judgeName.valueChanges.subscribe(judgeUserId => {
-      this.addJudge(judgeUserId);
-      this.isJudgeSelected = judgeUserId !== null;
-      this.canNavigate = this.isJudgeSelected;
-    }));
+    if (this.judgeName) {
+      this.$subscriptions.push(this.judgeName.valueChanges.subscribe(judgeUserId => {
+        this.addJudge(judgeUserId);
+        this.isJudgeSelected = judgeUserId !== null;
+        this.canNavigate = this.isJudgeSelected;
+      }));
+    }
 
-    this.$subscriptions.push(this.judgeDisplayName.valueChanges.subscribe(name => {
-      this.judge.display_name = name;
-    }));
+    if (this.judgeDisplayName) {
+      this.$subscriptions.push(this.judgeDisplayName.valueChanges.subscribe(name => {
+        this.judge.display_name = name;
+      }));
+    }
   }
 
   get judgeName() { return this.form.get('judgeName'); }
