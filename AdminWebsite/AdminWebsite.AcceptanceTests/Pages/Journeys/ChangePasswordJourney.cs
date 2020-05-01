350d98dc4976fe0fcfc18ad78b0ddc9f2eaa68b4
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-05-01 17:09:17 +0200

    feature/VIH-5416 : Added acceptance tests for Reset Password feature

---
+++ AdminWebsite/AdminWebsite.AcceptanceTests/Pages/Journeys/ChangePasswordJourney.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+
+namespace AdminWebsite.AcceptanceTests.Pages.Journeys
+{
+    public class ChangePasswordJourney : IJourney
+    {
+        public List<Page> Journey()
+        {
+            return new List<Page>()
+            {
+                Page.Login,
+                Page.Dashboard,
+                Page.ChangePassword
+            };
+        }
+
+        public void VerifyUserIsApplicableToJourney(string currentUserRole)
+        {
+            currentUserRole.ToLower().Should().BeOneOf("video hearings officer");
+        }
+
+        public void VerifyDestinationIsInThatJourney(Page destinationPage)
+        {
+            Journey().Should().Contain(destinationPage);
+        }
+
+        public Page GetNextPage(Page currentPage)
+        {
+            return Journey()[Journey().IndexOf(currentPage) + 1];
+        }
+    }
+}
