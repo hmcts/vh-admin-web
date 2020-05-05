565ef943170aa50458f6c10817c9984b6c062e25
Atif Rehman <atif.rehman@hmcts.net>
2020-05-05 17:18:52 +0100

    feature/VIH-5331 : Add tests

---
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/get-audio-file/hearing-search-results/hearing-search-results.component.spec.ts
@@ -0,0 +1,30 @@
+import { HearingSearchResultsComponent } from './hearing-search-results.component';
+import { HearingAudioSearchModel } from '../../common/model/hearing-audio-search-model';
+
+describe('HearingSearchResultsComponent', () => {
+    let component: HearingSearchResultsComponent;
+
+    beforeEach(() => {
+        component = new HearingSearchResultsComponent();
+    });
+
+    it('should return has results as false', function () {
+        expect(component.hasResults).toBeFalsy();
+    });
+
+    it('should return has results as false', function () {
+        component.results = [];
+        expect(component.hasResults).toBeFalsy();
+    });
+
+    it('should return has results as true', function () {
+        component.results = [
+            new HearingAudioSearchModel({
+                init(_data?: any): void {},
+                toJSON(data?: any): any {},
+                id: '363725D0-E3D6-4D4A-8D0A-E8E57575FBC2'
+            })
+        ];
+        expect(component.hasResults).toBeTruthy();
+    });
+});
