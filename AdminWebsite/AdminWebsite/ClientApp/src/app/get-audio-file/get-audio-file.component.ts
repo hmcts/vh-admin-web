e35df6ab54503460fd6dbefa157bfc70314e63fa
Atif Rehman <atif.rehman@hmcts.net>
2020-05-06 12:28:10 +0100

    feature/VIH-5331 : Adding hover tooltip WIP

--- AdminWebsite/AdminWebsite/ClientApp/src/app/get-audio-file/get-audio-file.component.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/get-audio-file/get-audio-file.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Inject, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HearingAudioSearchModel } from '../common/model/hearing-audio-search-model';
 import { AudioLinkService } from '../services/audio-link-service';
@@ -8,7 +8,7 @@ import { AudioLinkService } from '../services/audio-link-service';
     templateUrl: './get-audio-file.component.html',
     styleUrls: ['./get-audio-file.component.scss']
 })
-export class GetAudioFileComponent implements OnInit {
+export class GetAudioFileComponent implements OnInit, AfterViewInit {
     form: FormGroup;
     hasSearched: boolean;
     loadingData: boolean;
@@ -21,12 +21,37 @@ export class GetAudioFileComponent implements OnInit {
 
     async ngOnInit(): Promise<void> {
         this.form = this.fb.group({
-            caseNumber: ['', Validators.required]
+            caseNumber: ['11111', Validators.required]
         });
     }
 
     get caseNumber() {
         return this.form.get('caseNumber');
+    }
+
+    @ViewChild('tooltip', { static: false }) mySpan: ElementRef;
+
+    ngAfterViewInit() {
+        console.log('Hello ', this.mySpan.nativeElement);
+    }
+
+    onMouseMove(e) {
+        // console.log(e);
+        const x = e.layerX;
+        const y = e.layerY;
+        this.mySpan.nativeElement.style.top = y + -100 + 'px';
+        this.mySpan.nativeElement.style.left = x + 100 + 'px';
+    }
+
+    onMouseOver(e) {
+        console.log(e);
+        this.mySpan.nativeElement.style.display = 'block';
+        // this.mySpan.nativeElement.style.top = e.offsetY;
+        // this.mySpan.nativeElement.style.left = e.offsetX;
+    }
+
+    onMouseLeave() {
+        this.mySpan.nativeElement.style.display = 'none';
     }
 
     async search() {
