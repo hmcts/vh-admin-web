879164d35472ffa641468779deb8330f86f0a070
Atif Rehman <atif.rehman@hmcts.net>
2020-06-18 15:43:11 +0100

    feature/VIH-5785 : Change audio recording allowed to edit (#311)

--- AdminWebsite/AdminWebsite/ClientApp/src/app/booking/assign-judge/assign-judge.component.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/booking/assign-judge/assign-judge.component.ts
@@ -15,268 +15,274 @@ import { SanitizeInputText } from '../../common/formatters/sanitize-input-text';
 import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-assign-judge',
-  templateUrl: './assign-judge.component.html',
-  styleUrls: ['./assign-judge.component.css']
+    selector: 'app-assign-judge',
+    templateUrl: './assign-judge.component.html',
+    styleUrls: ['./assign-judge.component.css']
 })
-
 export class AssignJudgeComponent extends BookingBaseComponent implements OnInit, OnDestroy {
+    hearing: HearingModel;
+    judge: JudgeResponse;
+    judgeDisplayName: FormControl;
+    audioChoice: FormControl;
+    failedSubmission: boolean;
+    attemptingCancellation = false;
+    attemptingDiscardChanges = false;
+    hasSaved: boolean;
+    canNavigate = false;
 
-  hearing: HearingModel;
-  judge: JudgeResponse;
-  judgeDisplayName: FormControl;
-  audioChoice: FormControl;
-  failedSubmission: boolean;
-  attemptingCancellation = false;
-  attemptingDiscardChanges = false;
-  hasSaved: boolean;
-  canNavigate = false;
+    constants = Constants;
+    availableJudges: JudgeResponse[];
+    isJudgeSelected = true;
+    expanded = false;
+    $subscriptions: Subscription[] = [];
+    audioRecording = true;
 
-  constants = Constants;
-  availableJudges: JudgeResponse[];
-  isJudgeSelected = true;
-  expanded = false;
-  $subscriptions: Subscription[] = [];
-  audioRecording = true;
-  audioOptionsEnabled = true;
-
-  constructor(
-    private fb: FormBuilder,
-    protected router: Router,
-    protected hearingService: VideoHearingsService,
-    private judgeService: JudgeDataService,
-    protected bookingService: BookingService,
-    private logger: Logger) {
-    super(bookingService, router, hearingService);
-  }
-
-  static mapJudge(judge: ParticipantModel): JudgeResponse {
-    return new JudgeResponse({
-      email: judge.email,
-      first_name: judge.first_name,
-      last_name: judge.last_name,
-      display_name: judge.display_name
-    });
-  }
-
-  static mapJudgeToModel(judge: JudgeResponse): ParticipantModel {
-    const newParticipant = new ParticipantModel();
-    newParticipant.title = 'Judge';
-    newParticipant.first_name = judge.first_name;
-    newParticipant.middle_names = '';
-    newParticipant.last_name = judge.last_name;
-    newParticipant.display_name = judge.display_name;
-    newParticipant.email = judge.email;
-    newParticipant.is_judge = true;
-    newParticipant.phone = '';
-    newParticipant.id = null;
-    newParticipant.username = judge.email;
-    newParticipant.case_role_name = 'Judge';
-    newParticipant.hearing_role_name = 'Judge';
-    newParticipant.housenumber = null;
-    newParticipant.street = null;
-    newParticipant.city = null;
-    newParticipant.county = null;
-    newParticipant.postcode = null;
-    return newParticipant;
-  }
-
-  ngOnInit() {
-    this.failedSubmission = false;
-    this.checkForExistingRequest();
-    this.loadJudges();
-    this.initForm();
-    super.ngOnInit();
-  }
-
-  private checkForExistingRequest() {
-    this.hearing = this.hearingService.getCurrentRequest();
-  }
-
-  private initForm() {
-    const find_judge = this.hearing.participants.find(x => x.is_judge === true);
-    if (!find_judge) {
-      this.judge = new JudgeResponse({ email: this.constants.PleaseSelect, display_name: '' });
-    } else {
-      this.judge = AssignJudgeComponent.mapJudge(find_judge);
-      this.canNavigate = true;
-    }
-    this.judgeDisplayName = new FormControl(this.judge.display_name, {
-      validators: [
-        Validators.required,
-        Validators.pattern(Constants.TextInputPattern),
-        Validators.maxLength(255)
-      ], updateOn: 'blur'
-    });
-
-    if (this.hearing.audio_recording_required === null || this.hearing.audio_recording_required === undefined) {
-      this.hearing.audio_recording_required = true;
+    constructor(
+        private fb: FormBuilder,
+        protected router: Router,
+        protected hearingService: VideoHearingsService,
+        private judgeService: JudgeDataService,
+        protected bookingService: BookingService,
+        private logger: Logger
+    ) {
+        super(bookingService, router, hearingService);
     }
 
-    this.audioOptionsEnabled = this.hearing.status !== 'Created';
-
-    this.audioRecording = this.setInitialAudio();
-    this.audioChoice = new FormControl(this.audioRecording, Validators.required);
-
-    this.form = this.fb.group({
-      judgeName: [this.judge.email, Validators.required],
-      judgeDisplayName: this.judgeDisplayName,
-      audioChoice: this.audioChoice
-    });
-
-    this.$subscriptions.push(this.judgeName.valueChanges.subscribe(judgeUserId => {
-      this.addJudge(judgeUserId);
-      this.isJudgeSelected = judgeUserId !== null;
-      this.canNavigate = this.isJudgeSelected;
-    }));
-
-    this.$subscriptions.push(this.judgeDisplayName.valueChanges.subscribe(name => {
-      this.judge.display_name = name;
-    }));
-  }
-
-  private setInitialAudio() {
-    return this.hearing && this.hearing.audio_recording_required !== null && this.hearing.audio_recording_required !== undefined
-      ? this.hearing.audio_recording_required : true;
-  }
-
-  get judgeName() { return this.form.get('judgeName'); }
-
-
-  get judgeNameInvalid() {
-    return this.judgeName.invalid && (this.judgeName.dirty || this.judgeName.touched || this.failedSubmission);
-  }
-
-  get judgeDisplayNameInvalid() {
-    return this.judgeDisplayName.invalid && (this.judgeDisplayName.dirty || this.judgeDisplayName.touched || this.failedSubmission);
-  }
-
-  public addJudge(judgeId: string) {
-    if (judgeId) {
-      const selectedJudge = this.availableJudges.find(j => j.email === judgeId);
-      this.judge.first_name = selectedJudge.first_name;
-      this.judge.last_name = selectedJudge.last_name;
-      this.judge.email = selectedJudge.email;
-      if (!this.isJudgeDisplayNameSet()) {
-        this.judge.display_name = selectedJudge.display_name;
-      }
-      this.judgeDisplayName.patchValue(this.judge.display_name);
-      const newJudge = AssignJudgeComponent.mapJudgeToModel(this.judge);
-
-      const indexOfJudge = this.hearing.participants.findIndex(x => x.is_judge === true);
-      if (indexOfJudge > -1) {
-        this.hearing.participants.splice(indexOfJudge, 1);
-      }
-      this.hearing.participants.unshift(newJudge);
-    }
-  }
-
-  isJudgeDisplayNameSet(): boolean {
-    let result = false;
-    if (this.judge && this.judge.display_name) {
-      const enteredJudge = this.availableJudges.find(j => j.display_name === this.judge.display_name);
-      result = !enteredJudge;
-    }
-    return result;
-  }
-
-  changeDisplayName() {
-    if (this.judge && this.judge.display_name) {
-      const indexOfJudge = this.hearing.participants.findIndex(x => x.is_judge === true);
-      if (indexOfJudge !== -1) {
-        this.hearing.participants[indexOfJudge].display_name = this.judge.display_name;
-      }
-    }
-    const text = SanitizeInputText(this.judgeDisplayName.value);
-    this.judgeDisplayName.setValue(text);
-  }
-
-  saveJudge() {
-    if (!this.judge.email || this.judge.email === this.constants.PleaseSelect) {
-      this.isJudgeSelected = false;
-      return;
-    }
-    if (!this.judge.display_name) {
-      this.failedSubmission = true;
-      return;
+    static mapJudge(judge: ParticipantModel): JudgeResponse {
+        return new JudgeResponse({
+            email: judge.email,
+            first_name: judge.first_name,
+            last_name: judge.last_name,
+            display_name: judge.display_name
+        });
     }
 
-    if (this.form.valid) {
-      this.failedSubmission = false;
-      this.form.markAsPristine();
-      this.hasSaved = true;
-      this.hearing.audio_recording_required = this.audioChoice.value;
-      this.changeDisplayName();
-      this.hearingService.updateHearingRequest(this.hearing);
-      if (this.editMode) {
-        this.router.navigate([PageUrls.Summary]);
-      } else {
-        this.router.navigate([PageUrls.AddParticipants]);
-      }
-    } else {
-      this.failedSubmission = true;
+    static mapJudgeToModel(judge: JudgeResponse): ParticipantModel {
+        const newParticipant = new ParticipantModel();
+        newParticipant.title = 'Judge';
+        newParticipant.first_name = judge.first_name;
+        newParticipant.middle_names = '';
+        newParticipant.last_name = judge.last_name;
+        newParticipant.display_name = judge.display_name;
+        newParticipant.email = judge.email;
+        newParticipant.is_judge = true;
+        newParticipant.phone = '';
+        newParticipant.id = null;
+        newParticipant.username = judge.email;
+        newParticipant.case_role_name = 'Judge';
+        newParticipant.hearing_role_name = 'Judge';
+        newParticipant.housenumber = null;
+        newParticipant.street = null;
+        newParticipant.city = null;
+        newParticipant.county = null;
+        newParticipant.postcode = null;
+        return newParticipant;
     }
-  }
 
-  confirmCancelBooking() {
-    if (this.editMode) {
-      if (this.form.dirty || this.form.touched) {
-        this.attemptingDiscardChanges = true;
-      } else {
-        this.router.navigate([PageUrls.Summary]);
-      }
-    } else {
-      this.attemptingCancellation = true;
+    ngOnInit() {
+        this.failedSubmission = false;
+        this.checkForExistingRequest();
+        this.loadJudges();
+        this.initForm();
+        super.ngOnInit();
     }
-  }
 
-  continueBooking() {
-    this.attemptingCancellation = false;
-    this.attemptingDiscardChanges = false;
-  }
+    private checkForExistingRequest() {
+        this.hearing = this.hearingService.getCurrentRequest();
+    }
 
-  cancelAssignJudge() {
-    this.attemptingCancellation = false;
-    this.form.reset();
-    this.hearingService.cancelRequest();
-    this.router.navigate([PageUrls.Dashboard]);
-  }
+    private initForm() {
+        const find_judge = this.hearing.participants.find((x) => x.is_judge === true);
+        if (!find_judge) {
+            this.judge = new JudgeResponse({ email: this.constants.PleaseSelect, display_name: '' });
+        } else {
+            this.judge = AssignJudgeComponent.mapJudge(find_judge);
+            this.canNavigate = true;
+        }
+        this.judgeDisplayName = new FormControl(this.judge.display_name, {
+            validators: [Validators.required, Validators.pattern(Constants.TextInputPattern), Validators.maxLength(255)],
+            updateOn: 'blur'
+        });
 
-  cancelChanges() {
-    this.attemptingDiscardChanges = false;
-    this.form.reset();
-    this.navigateToSummary();
-  }
+        if (this.hearing.audio_recording_required === null || this.hearing.audio_recording_required === undefined) {
+            this.hearing.audio_recording_required = true;
+        }
 
-  goToDiv(fragment: string): void {
-    window.document.getElementById(fragment).parentElement.parentElement.scrollIntoView();
-  }
+        this.audioRecording = this.setInitialAudio();
+        this.audioChoice = new FormControl(this.audioRecording, Validators.required);
 
-  private loadJudges() {
-    if (this.availableJudges) { return; }
-    this.$subscriptions.push(this.judgeService.getJudges()
-      .subscribe(
-        (data: JudgeResponse[]) => {
-          this.availableJudges = data.filter(x => x.first_name && x.last_name);
-          const userResponse = new JudgeResponse();
-          userResponse.email = this.constants.PleaseSelect;
-          userResponse.display_name = '';
-          this.availableJudges.unshift(userResponse);
-        },
-        error => this.onErrorLoadJudges(error)
-      ));
-  }
+        this.form = this.fb.group({
+            judgeName: [this.judge.email, Validators.required],
+            judgeDisplayName: this.judgeDisplayName,
+            audioChoice: this.audioChoice
+        });
 
-  onErrorLoadJudges(error) {
-    this.logger.error('Error to get list of judges.', error);
-  }
+        this.$subscriptions.push(
+            this.judgeName.valueChanges.subscribe((judgeUserId) => {
+                this.addJudge(judgeUserId);
+                this.isJudgeSelected = judgeUserId !== null;
+                this.canNavigate = this.isJudgeSelected;
+            })
+        );
 
-  toggle() {
-    this.expanded = !this.expanded;
-  }
+        this.$subscriptions.push(
+            this.judgeDisplayName.valueChanges.subscribe((name) => {
+                this.judge.display_name = name;
+            })
+        );
+    }
 
-  ngOnDestroy() {
-    this.bookingService.removeEditMode();
-    this.$subscriptions.forEach(subcription => { if (subcription) { subcription.unsubscribe(); } });
-  }
+    private setInitialAudio() {
+        return this.hearing && this.hearing.audio_recording_required !== null && this.hearing.audio_recording_required !== undefined
+            ? this.hearing.audio_recording_required
+            : true;
+    }
+
+    get judgeName() {
+        return this.form.get('judgeName');
+    }
+
+    get judgeNameInvalid() {
+        return this.judgeName.invalid && (this.judgeName.dirty || this.judgeName.touched || this.failedSubmission);
+    }
+
+    get judgeDisplayNameInvalid() {
+        return this.judgeDisplayName.invalid && (this.judgeDisplayName.dirty || this.judgeDisplayName.touched || this.failedSubmission);
+    }
+
+    public addJudge(judgeId: string) {
+        if (judgeId) {
+            const selectedJudge = this.availableJudges.find((j) => j.email === judgeId);
+            this.judge.first_name = selectedJudge.first_name;
+            this.judge.last_name = selectedJudge.last_name;
+            this.judge.email = selectedJudge.email;
+            if (!this.isJudgeDisplayNameSet()) {
+                this.judge.display_name = selectedJudge.display_name;
+            }
+            this.judgeDisplayName.patchValue(this.judge.display_name);
+            const newJudge = AssignJudgeComponent.mapJudgeToModel(this.judge);
+
+            const indexOfJudge = this.hearing.participants.findIndex((x) => x.is_judge === true);
+            if (indexOfJudge > -1) {
+                this.hearing.participants.splice(indexOfJudge, 1);
+            }
+            this.hearing.participants.unshift(newJudge);
+        }
+    }
+
+    isJudgeDisplayNameSet(): boolean {
+        let result = false;
+        if (this.judge && this.judge.display_name) {
+            const enteredJudge = this.availableJudges.find((j) => j.display_name === this.judge.display_name);
+            result = !enteredJudge;
+        }
+        return result;
+    }
+
+    changeDisplayName() {
+        if (this.judge && this.judge.display_name) {
+            const indexOfJudge = this.hearing.participants.findIndex((x) => x.is_judge === true);
+            if (indexOfJudge !== -1) {
+                this.hearing.participants[indexOfJudge].display_name = this.judge.display_name;
+            }
+        }
+        const text = SanitizeInputText(this.judgeDisplayName.value);
+        this.judgeDisplayName.setValue(text);
+    }
+
+    saveJudge() {
+        if (!this.judge.email || this.judge.email === this.constants.PleaseSelect) {
+            this.isJudgeSelected = false;
+            return;
+        }
+        if (!this.judge.display_name) {
+            this.failedSubmission = true;
+            return;
+        }
+
+        if (this.form.valid) {
+            this.failedSubmission = false;
+            this.form.markAsPristine();
+            this.hasSaved = true;
+            this.hearing.audio_recording_required = this.audioChoice.value;
+            this.changeDisplayName();
+            this.hearingService.updateHearingRequest(this.hearing);
+            if (this.editMode) {
+                this.router.navigate([PageUrls.Summary]);
+            } else {
+                this.router.navigate([PageUrls.AddParticipants]);
+            }
+        } else {
+            this.failedSubmission = true;
+        }
+    }
+
+    confirmCancelBooking() {
+        if (this.editMode) {
+            if (this.form.dirty || this.form.touched) {
+                this.attemptingDiscardChanges = true;
+            } else {
+                this.router.navigate([PageUrls.Summary]);
+            }
+        } else {
+            this.attemptingCancellation = true;
+        }
+    }
+
+    continueBooking() {
+        this.attemptingCancellation = false;
+        this.attemptingDiscardChanges = false;
+    }
+
+    cancelAssignJudge() {
+        this.attemptingCancellation = false;
+        this.form.reset();
+        this.hearingService.cancelRequest();
+        this.router.navigate([PageUrls.Dashboard]);
+    }
+
+    cancelChanges() {
+        this.attemptingDiscardChanges = false;
+        this.form.reset();
+        this.navigateToSummary();
+    }
+
+    goToDiv(fragment: string): void {
+        window.document.getElementById(fragment).parentElement.parentElement.scrollIntoView();
+    }
+
+    private loadJudges() {
+        if (this.availableJudges) {
+            return;
+        }
+        this.$subscriptions.push(
+            this.judgeService.getJudges().subscribe(
+                (data: JudgeResponse[]) => {
+                    this.availableJudges = data.filter((x) => x.first_name && x.last_name);
+                    const userResponse = new JudgeResponse();
+                    userResponse.email = this.constants.PleaseSelect;
+                    userResponse.display_name = '';
+                    this.availableJudges.unshift(userResponse);
+                },
+                (error) => this.onErrorLoadJudges(error)
+            )
+        );
+    }
+
+    onErrorLoadJudges(error) {
+        this.logger.error('Error to get list of judges.', error);
+    }
+
+    toggle() {
+        this.expanded = !this.expanded;
+    }
+
+    ngOnDestroy() {
+        this.bookingService.removeEditMode();
+        this.$subscriptions.forEach((subcription) => {
+            if (subcription) {
+                subcription.unsubscribe();
+            }
+        });
+    }
 }
