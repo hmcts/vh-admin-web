e9271aa0d62ede346aa18456e001eadb724ec6b4
Lionel Roche <lionel.roche@hmcts.net>
2020-09-03 20:11:49 +0100

    VIH-6312-VHO_saves_hearing_with_Video_access_points_added - updated ui to include copy sip to clip bard from booking details screen

---
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/bookings-list/copy-sip/copy-sip.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
+import { EndpointModel } from 'src/app/common/model/endpoint.model';
+import { ClipboardService } from 'ngx-clipboard';
+
+@Component({
+  selector: 'app-copy-sip',
+  templateUrl: './copy-sip.component.html',
+  styleUrls: ['./copy-sip.component.scss']
+})
+export class CopySipComponent implements OnInit {
+  @Input() endpoint: EndpointModel;
+  displayTooltip: boolean;
+  tooltip: string;
+  @ViewChild('sipAddress', { static: false }) sipAddress: ElementRef;
+
+  constructor(private clipboardService: ClipboardService) { }
+
+  ngOnInit(): void {
+    this.displayTooltip = true;
+    this.tooltip = 'Copy SIP address';
+  }
+
+  onMouseOver($event: MouseEvent): void {
+    if (!this.sipAddress) {
+      return;
+    }
+    const x = $event.clientX;
+    const y = $event.clientY;
+    const elem = this.sipAddress.nativeElement as HTMLDivElement;
+
+    elem.style.left = x + 20 + 'px';
+    this.setTooltipVisibility(false);
+  }
+
+  copyToClipboard(endpoint: EndpointModel) {
+    this.clipboardService.copyFromContent(endpoint.sip);
+    this.tooltip = 'SIP address copied to clipboard';
+    this.setTooltipVisibility(false);
+  }
+
+  onMouseOut(): void {
+    this.setTooltipVisibility(true);
+  }
+
+  setTooltipVisibility(visible: boolean) {
+    this.displayTooltip = visible;
+  }
+}
