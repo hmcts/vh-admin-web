84fa8a507cbd0085eb92a3e561980e8948b0c857
Shaed Parkar <shaed.parkar@slimsolutions.company>
2020-09-03 12:52:35 +0100

    VIH-6274 open hearing from delete search list

--- AdminWebsite/AdminWebsite/ClientApp/src/app/delete-participant/delete-participant-search-results/delete-participant-search-results.component.spec.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/delete-participant/delete-participant-search-results/delete-participant-search-results.component.spec.ts
@@ -1,12 +1,25 @@
+import { Router } from '@angular/router';
 import { ParticipantHearingDeleteResultModel } from 'src/app/common/model/participant-hearing-delete-result-model';
+import { BookingPersistService } from 'src/app/services/bookings-persist.service';
 import { HearingsByUsernameForDeletionResponse } from 'src/app/services/clients/api-client';
+import { VideoHearingsService } from 'src/app/services/video-hearings.service';
+import { PageUrls } from 'src/app/shared/page-url.constants';
 import { DeleteParticipantSearchResultsComponent } from './delete-participant-search-results.component';
 
 describe('DeleteParticipantSearchResultsComponent', () => {
     let component: DeleteParticipantSearchResultsComponent;
+    let bookingPersistService: jasmine.SpyObj<BookingPersistService>;
+    let videoHearingService: jasmine.SpyObj<VideoHearingsService>;
+    let router: jasmine.SpyObj<Router>;
+
+    beforeAll(() => {
+        bookingPersistService = jasmine.createSpyObj<BookingPersistService>('BookingPersistService', ['selectedHearingId']);
+        videoHearingService = jasmine.createSpyObj<VideoHearingsService>('VideoHearingsService', ['cancelRequest']);
+        router = jasmine.createSpyObj<Router>(['navigate']);
+    });
 
     beforeEach(() => {
-        component = new DeleteParticipantSearchResultsComponent();
+        component = new DeleteParticipantSearchResultsComponent(bookingPersistService, videoHearingService, router);
     });
 
     it('should return true when results list is greater than 0', () => {
@@ -37,5 +50,11 @@ describe('DeleteParticipantSearchResultsComponent', () => {
     it('should return userNotFound false when results is set ', () => {
         component.results = [];
         expect(component.userNotFound).toBeTruthy();
+    });
+
+    it('should navigate to booking details for edit', () => {
+        component.editHearing('1234');
+        expect(videoHearingService.cancelRequest).toHaveBeenCalled();
+        expect(router.navigate).toHaveBeenCalledWith([PageUrls.BookingDetails]);
     });
 });
