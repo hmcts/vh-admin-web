43e7284e45989c1f14cc77561b6da0516839c618
Valentina <Valentina.bennett@hmcts.net>
2020-09-25 12:50:46 +0100

    feature/VIH-6549 : removed unused code

--- AdminWebsite/AdminWebsite/ClientApp/src/app/get-audio-file/get-audio-link-button/multi-audio-link-model.ts
+++
@@ -1,9 +0,0 @@
-export class MultiAudioLinkModel {
-    constructor(linkIndex, selected) {
-        this.linkIndex = linkIndex;
-        this.selected = selected;
-    }
-
-    linkIndex: number;
-    selected: boolean;
-}
