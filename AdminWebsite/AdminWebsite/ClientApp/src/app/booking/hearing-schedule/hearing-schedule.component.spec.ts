a87bc69e53b2a18958e4a5de9dc5f8f7d37cb2b5
Valentina <Valentina.bennett@hmcts.net>
2020-09-29 11:55:50 +0100

    feature/VIH-6281 : fix linting

--- AdminWebsite/AdminWebsite/ClientApp/src/app/booking/hearing-schedule/hearing-schedule.component.spec.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/booking/hearing-schedule/hearing-schedule.component.spec.ts
@@ -53,7 +53,7 @@ function initFormControls(component: HearingScheduleComponent) {
 }
 
 function addDays(date, days) {
-    var result = new Date(date);
+    const result = new Date(date);
     result.setDate(result.getDate() + days);
     return result;
 }
--- AdminWebsite/AdminWebsite/ClientApp/src/app/booking/summary/summary.component.spec.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/booking/summary/summary.component.spec.ts
@@ -59,7 +59,7 @@ function initExistingHearingRequest(): HearingModel {
 }
 
 function addDays(date, days) {
-    var result = new Date(date);
+    const result = new Date(date);
     result.setDate(result.getDate() + days);
     return result;
 }
