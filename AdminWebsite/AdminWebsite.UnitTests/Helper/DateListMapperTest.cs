2aac15da467f89f7c15a7fdb8fd4e410a1771be1
Valentina <40822624+valentinabennett@users.noreply.github.com>
2020-10-12 11:18:07 +0100

    feature/VIH-6604 : Fix for multi dates hearing (#472)

--- AdminWebsite/AdminWebsite.UnitTests/Helper/DateListMapperTest.cs
+++ AdminWebsite/AdminWebsite.UnitTests/Helper/DateListMapperTest.cs
@@ -11,8 +11,8 @@ namespace AdminWebsite.UnitTests.Helper
         [Test]
         public void Should_return_range_of_dates_not_included_weekends()
         {
-            var startDate = new DateTime(2020, 10, 1);
-            var endDate = new DateTime(2020, 10, 6);
+            var startDate = new DateTime(2020, 10, 1, 4, 30, 0, 0);
+            var endDate = new DateTime(2020, 10, 6, 4, 35, 0, 0);
             var expectDays = 3;
 
             var result = DateListMapper.GetListOfWorkingDates(startDate, endDate);
