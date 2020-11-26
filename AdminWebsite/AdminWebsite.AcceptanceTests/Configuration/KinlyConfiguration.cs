d3ef0b5dab9608ba1225a7662d8584ff7ea7e3a9
Valentina <40822624+valentinabennett@users.noreply.github.com>
2020-11-26 15:40:52 +0000

    VIH-6854 Add phone to kinly config group pipeline (#540)
    * feature/VIH-6854 : Add phone to kinly config group pipline
    
    * feature/VIH-6854 : removed old setting from pipeline for phone

---
+++ AdminWebsite/AdminWebsite.AcceptanceTests/Configuration/KinlyConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace AdminWebsite.AcceptanceTests.Configuration
+{
+    public class KinlyConfiguration
+    {
+        /// <summary>
+        /// Conference phone number for audio only hearing
+        /// </summary>
+        public string ConferencePhoneNumber { get; set; }
+    }
+}
