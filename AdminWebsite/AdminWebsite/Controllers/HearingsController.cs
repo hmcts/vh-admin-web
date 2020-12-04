1ca85b5b658f45b847e6b1f02c8cd949596a15e1
Atif rehman <atif.rehman@outlook.com>
2020-12-04 12:53:14 +0000

    feature/VIH-6711 : increase_confirm_retry

--- AdminWebsite/AdminWebsite/Controllers/HearingsController.cs
+++ AdminWebsite/AdminWebsite/Controllers/HearingsController.cs
@@ -541,7 +541,7 @@ namespace AdminWebsite.Controllers
                 {
                     var conferenceDetailsResponse = await _pollyRetryService.WaitAndRetryAsync<VideoApiException, ConferenceDetailsResponse>
                     (
-                        6, _ => TimeSpan.FromSeconds(5),
+                        6, _ => TimeSpan.FromSeconds(8),
                         retryAttempt => _logger.LogWarning($"Failed to retrieve conference details from the VideoAPi for hearingId {hearingId}. Retrying attempt {retryAttempt}"),
                         videoApiResponseObject => !ConferenceExistsWithMeetingRoom(videoApiResponseObject),
                         () => _videoApiClient.GetConferenceByHearingRefIdAsync(hearingId)
