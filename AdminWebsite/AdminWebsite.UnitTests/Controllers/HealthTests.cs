adf049ac82b7c6941a36b7fc01cc65902006529b
Sunil D'Souza <41689433+jdssunil@users.noreply.github.com>
2020-12-14 20:12:15 +0000

    VIH-6763 Connect AdminWeb to VH-Notify-API (#541)
    * feature/VIH-6763 : Connect AdminWeb to VH-Notify-API
    
    * feature/VIH-6763 : Pipeline changes
    
    * feature/VIH-6763 : Update yml files
    
    * feature/VIH-6763 : Notification client connection
    
    * feature/VIH-6763 - More unit tests and refactoring

--- AdminWebsite/AdminWebsite.UnitTests/Controllers/HealthTests.cs
+++ AdminWebsite/AdminWebsite.UnitTests/Controllers/HealthTests.cs
@@ -12,6 +12,9 @@ using System.Net;
 using System.Threading.Tasks;
 using AdminWebsite.VideoAPI.Client;
 using HealthCheckResponse = AdminWebsite.Models.HealthCheckResponse;
+using NotificationApi.Client;
+using NotificationApi.Contract;
+using Microsoft.Extensions.Logging;
 
 namespace AdminWebsite.UnitTests.Controllers
 {
@@ -21,15 +24,18 @@ namespace AdminWebsite.UnitTests.Controllers
         private Mock<IUserApiClient> _userApiClientMock;
         private Mock<IBookingsApiClient> _bookingsApiClientMock;
         private Mock<IVideoApiClient> _videoApiClientMock;
-
+        private Mock<INotificationApiClient> _notificationApiClientMock;
+        
         [SetUp]
         public void Setup()
         {
             _userApiClientMock = new Mock<IUserApiClient>();
             _bookingsApiClientMock = new Mock<IBookingsApiClient>();
             _videoApiClientMock = new Mock<IVideoApiClient>();
+            _notificationApiClientMock = new Mock<INotificationApiClient>();
 
-            _controller = new HealthCheckController(_userApiClientMock.Object, _bookingsApiClientMock.Object, _videoApiClientMock.Object);
+            _controller = new HealthCheckController(_userApiClientMock.Object, _bookingsApiClientMock.Object, _videoApiClientMock.Object,
+                                _notificationApiClientMock.Object, new Mock<ILogger<HealthCheckController>>().Object);
 
             var judges = Builder<UserResponse>.CreateListOfSize(3).Build().ToList();
             
@@ -45,14 +51,25 @@ namespace AdminWebsite.UnitTests.Controllers
                 .Setup(x => x.GetJudgesAsync())
                 .ThrowsAsync(exception);
 
-            var result = await _controller.Health();
-            var typedResult = (ObjectResult) result;
-            typedResult.StatusCode.Should().Be((int) HttpStatusCode.InternalServerError);
-            var response = (HealthCheckResponse) typedResult.Value;
+            var response = await GetResponseFromHealthCheck(HttpStatusCode.InternalServerError);
             response.UserApiHealth.Successful.Should().BeFalse();
             response.UserApiHealth.ErrorMessage.Should().NotBeNullOrWhiteSpace();
         }
-        
+
+        [Test]
+        public async Task Should_return_user_api_exception_when_user_api_not_found()
+        {
+            var exception = new UserServiceException("User api error", 404, "response", null, new Exception());
+
+            _userApiClientMock
+                .Setup(x => x.GetJudgesAsync())
+                .ThrowsAsync(exception);
+
+            var response = await GetResponseFromHealthCheck();
+            response.UserApiHealth.Successful.Should().BeTrue();
+            response.UserApiHealth.ErrorMessage.Should().BeNullOrWhiteSpace();
+        }
+
         [Test]
         public async Task Should_return_internal_server_error_result_when_bookings_api_not_reachable()
         {
@@ -62,10 +79,7 @@ namespace AdminWebsite.UnitTests.Controllers
                 .Setup(x => x.GetCaseTypesAsync())
                 .ThrowsAsync(exception);
 
-            var result = await _controller.Health();
-            var typedResult = (ObjectResult) result;
-            typedResult.StatusCode.Should().Be((int) HttpStatusCode.InternalServerError);
-            var response = (HealthCheckResponse) typedResult.Value;
+            var response = await GetResponseFromHealthCheck(HttpStatusCode.InternalServerError);
             response.BookingsApiHealth.Successful.Should().BeFalse();
             response.BookingsApiHealth.ErrorMessage.Should().NotBeNullOrWhiteSpace();
         }
@@ -79,14 +93,25 @@ namespace AdminWebsite.UnitTests.Controllers
                 .Setup(x => x.GetCaseTypesAsync())
                 .ThrowsAsync(exception);
 
-            var result = await _controller.Health();
-            var typedResult = (ObjectResult) result;
-            typedResult.StatusCode.Should().Be((int) HttpStatusCode.InternalServerError);
-            var response = (HealthCheckResponse) typedResult.Value;
+            var response = await GetResponseFromHealthCheck(HttpStatusCode.InternalServerError);
             response.BookingsApiHealth.Successful.Should().BeFalse();
             response.BookingsApiHealth.ErrorMessage.Should().NotBeNullOrWhiteSpace();
         }
-        
+
+        [Test]
+        public async Task Should_return_booking_api_exception_when_booking_api_not_found()
+        {
+            var exception = new BookingsApiException("Bookings api error", 404, "response", null, new Exception());
+
+            _bookingsApiClientMock
+                .Setup(x => x.GetCaseTypesAsync())
+                .ThrowsAsync(exception);
+
+            var response = await GetResponseFromHealthCheck();
+            response.BookingsApiHealth.Successful.Should().BeTrue();
+            response.BookingsApiHealth.ErrorMessage.Should().BeNullOrWhiteSpace();
+        }
+
         [Test]
         public async Task Should_return_internal_server_error_result_when_video_api_not_reachable()
         {
@@ -96,25 +121,61 @@ namespace AdminWebsite.UnitTests.Controllers
                 .Setup(x => x.GetExpiredOpenConferencesAsync())
                 .ThrowsAsync(exception);
 
-            var result = await _controller.Health();
-            var typedResult = (ObjectResult) result;
-            typedResult.StatusCode.Should().Be((int) HttpStatusCode.InternalServerError);
-            var response = (HealthCheckResponse) typedResult.Value;
+            var response = await GetResponseFromHealthCheck(HttpStatusCode.InternalServerError);
             response.VideoApiHealth.Successful.Should().BeFalse();
             response.VideoApiHealth.ErrorMessage.Should().NotBeNullOrWhiteSpace();
         }
 
         [Test]
+        public async Task Should_return_video_api_exception_when_video_api_not_found()
+        {
+            var exception = new VideoApiException("Video api error", 404, "response", null, new Exception());
+
+            _videoApiClientMock
+                .Setup(x => x.GetExpiredOpenConferencesAsync())
+                .ThrowsAsync(exception);
+
+            var response = await GetResponseFromHealthCheck();
+            response.VideoApiHealth.Successful.Should().BeTrue();
+            response.VideoApiHealth.ErrorMessage.Should().BeNullOrWhiteSpace();
+        }
+
+        [Test]
+        public async Task Should_return_internal_server_error_result_when_notification_api_not_reachable()
+        {
+            var exception = new AggregateException("Notification api error");
+
+            _notificationApiClientMock
+                .Setup(x => x.GetTemplateByNotificationTypeAsync(It.IsAny<NotificationType>()))
+                .ThrowsAsync(exception);
+
+            var response = await GetResponseFromHealthCheck(HttpStatusCode.InternalServerError);
+            response.NotificationApiHealth.Successful.Should().BeFalse();
+            response.NotificationApiHealth.ErrorMessage.Should().NotBeNullOrWhiteSpace();
+        }
+
+        [Test]
+        public async Task Should_return_notifiy_exception_when_notification_api_not_found()
+        {
+            var exception = new NotificationApiException("Notification api error", 404, "response", null, new Exception());
+
+            _notificationApiClientMock
+                .Setup(x => x.GetTemplateByNotificationTypeAsync(It.IsAny<NotificationType>()))
+                .ThrowsAsync(exception);
+
+            var response = await GetResponseFromHealthCheck();
+            response.NotificationApiHealth.Successful.Should().BeTrue();
+            response.NotificationApiHealth.ErrorMessage.Should().BeNullOrWhiteSpace();
+        }
+
+        [Test]
         public async Task Should_return_ok_when_all_services_are_running()
         {
-            var result = await _controller.Health();
-            var typedResult = (ObjectResult) result;
-            typedResult.StatusCode.Should().Be((int) HttpStatusCode.OK);
-            
-            var response = (HealthCheckResponse) typedResult.Value;
+            var response = await GetResponseFromHealthCheck();
             response.BookingsApiHealth.Successful.Should().BeTrue();
             response.UserApiHealth.Successful.Should().BeTrue();
             response.VideoApiHealth.Successful.Should().BeTrue();
+            response.NotificationApiHealth.Successful.Should().BeTrue();
         }
 
         [Test]
@@ -125,6 +186,14 @@ namespace AdminWebsite.UnitTests.Controllers
             var response = (HealthCheckResponse)typedResult.Value;
             response.AppVersion.FileVersion.Should().NotBeNullOrEmpty();
             response.AppVersion.InformationVersion.Should().NotBeNullOrEmpty();
+        }
+
+        private async Task<HealthCheckResponse> GetResponseFromHealthCheck(HttpStatusCode expectedStatusCode = HttpStatusCode.OK)
+        {
+            var result = await _controller.Health();
+            var typedResult = (ObjectResult)result;
+            typedResult.StatusCode.Should().Be((int)expectedStatusCode);
+            return (HealthCheckResponse)typedResult.Value;
         }
     }
 }
--- AdminWebsite/AdminWebsite/Configuration/ServiceSettings.cs
+++ AdminWebsite/AdminWebsite/Configuration/ServiceSettings.cs
@@ -8,7 +8,8 @@
         public string UserApiResourceId { get; set; }
         public string VideoApiUrl { get; set; }
         public string VideoApiResourceId { get; set; }
-
+        public string NotificationApiUrl { get; set; }
+        public string NotificationApiResourceId { get; set; }
         /// <summary>
         /// To join the conference phone number
         /// </summary>
