97f564c7a555c53b8ba7d0527c3893acae73eb8d
Saravanan Radhakrishnan <shravan_krish@yahoo.com>
2021-01-25 10:46:46 +0000

    VIH-5602 Mutation issue fix (#563)
    * Mutation issue fixes
    
    * HearingController mutation fixes
    
    * Mutation fix
    
    Co-authored-by: Saravanan Radha Krishnan <Saravanan.Krishnan@HMCTS.NET>

---
+++ AdminWebsite/AdminWebsite.UnitTests/Extensions/ConferenceDetailsResponseExtensionsTests.cs
@@ -0,0 +1,91 @@
+﻿
+using AdminWebsite.Extensions;
+using AdminWebsite.VideoAPI.Client;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+
+namespace AdminWebsite.UnitTests.Extensions
+{
+    public class ConferenceDetailsResponseExtensionsTests
+    {
+        private ConferenceDetailsResponse conferenceDetailsResp;
+
+        [SetUp]
+        public void Init()
+        {
+            conferenceDetailsResp = new ConferenceDetailsResponse
+            {
+                Id = Guid.NewGuid(),
+                Hearing_id = Guid.NewGuid(),
+                Meeting_room = new MeetingRoomResponse
+                {
+                    Admin_uri = "admin",
+                    Judge_uri = "judge",
+                    Participant_uri = "participant",
+                    Pexip_node = "pexip",
+                    Telephone_conference_id = "121212"
+                }
+            };
+        }
+
+        [Test]
+        public void Should_return_false_MeetingRoom_is_valid()
+        {  
+            var result = conferenceDetailsResp.HasInvalidMeetingRoom();
+
+            result.Should().BeFalse();
+        }
+
+        [Test]
+        public void Should_return_true_MeetingRoom_is_null()
+        {
+            conferenceDetailsResp.Meeting_room = null;
+
+            var result = conferenceDetailsResp.HasInvalidMeetingRoom();
+
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void Should_return_true_AdminUri_is_null_or_empty()
+        {
+            conferenceDetailsResp.Meeting_room.Admin_uri = string.Empty;
+
+            var result = conferenceDetailsResp.HasInvalidMeetingRoom();
+
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void Should_return_true_JudgeUri_is_null_or_empty()
+        {
+            conferenceDetailsResp.Meeting_room.Judge_uri = string.Empty;
+
+            var result = conferenceDetailsResp.HasInvalidMeetingRoom();
+
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void Should_return_true_ParticipantUri_is_null_or_empty()
+        {
+            conferenceDetailsResp.Meeting_room.Participant_uri = string.Empty;
+
+            var result = conferenceDetailsResp.HasInvalidMeetingRoom();
+
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void Should_return_true_PexipNode_is_null_or_empty()
+        {
+            conferenceDetailsResp.Meeting_room.Pexip_node = string.Empty;
+
+            var result = conferenceDetailsResp.HasInvalidMeetingRoom();
+
+            result.Should().BeTrue();
+        }
+
+    }
+}
---
+++ AdminWebsite/AdminWebsite/Extensions/UserProfileExtensions.cs
@@ -0,0 +1,20 @@
+﻿
+using AdminWebsite.Security;
+using AdminWebsite.UserAPI.Client;
+using System;
+
+namespace AdminWebsite.Extensions
+{
+    public static class UserProfileExtensions
+    {
+        public static bool HasValidUserRole(this UserProfile user)
+        {
+            if (string.IsNullOrWhiteSpace(user.User_role) || !Enum.TryParse<UserRoleType>(user.User_role, out var role) || role == UserRoleType.None)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
