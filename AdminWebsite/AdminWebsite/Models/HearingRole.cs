89143c3a87a2b373785eb970eff9712d06235620
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2021-02-12 17:50:21 +0100

    feature/VIH-7255 : Removed code smell

--- AdminWebsite/AdminWebsite/Models/HearingRole.cs
+++ AdminWebsite/AdminWebsite/Models/HearingRole.cs
@@ -1,20 +1,8 @@
-using System;
-
 namespace AdminWebsite.Models
 {
-    public class HearingRole : IComparable
+    public class HearingRole
     {
         public string Name { get; set; }
         public string UserRole { get; set; }
-        
-        public int CompareTo(object obj)
-        {
-            return obj switch
-            {
-                null => 1,
-                HearingRole hearingRole => string.Compare(Name, hearingRole.Name, StringComparison.Ordinal),
-                _ => 0
-            };
-        }
     }
 }
