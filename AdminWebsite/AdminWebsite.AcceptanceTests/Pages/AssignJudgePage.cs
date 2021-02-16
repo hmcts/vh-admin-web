b01a79e005cf5298c3d3ed1f83f8ac6c87f5c99a
williambowshersolirius <77436840+williambowshersolirius@users.noreply.github.com>
2021-02-16 14:06:46 +0000

    Added JudgeEmailFld and JudgePhoneFld to page in Acceptance tests

--- AdminWebsite/AdminWebsite.AcceptanceTests/Pages/AssignJudgePage.cs
+++ AdminWebsite/AdminWebsite.AcceptanceTests/Pages/AssignJudgePage.cs
@@ -5,7 +5,9 @@ namespace AdminWebsite.AcceptanceTests.Pages
     public static class AssignJudgePage
     {
         public static By JudgeNameDropdown = By.Id("judgeName");
-        public static By JudgeDisplayNameTextfield = By.Id("judgeDisplayName");
+        public static By JudgeDisplayNameTextfield = By.Id("judgeDisplayNameFld");
+        public static By JudgeEmailTextField = By.Id("judgeEmailFld");
+        public static By JudgePhoneTextField = By.Id("judgePhoneFld");
         public static By NextButton = By.Id("nextButton");
         public static By CancelButton = By.Id("cancelButton");
     }
