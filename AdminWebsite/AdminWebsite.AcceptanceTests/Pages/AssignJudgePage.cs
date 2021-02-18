5a9700aa39473cf03333e55b766460bc4e5b83d5
Saravanan Radha Krishnan <Saravanan.Krishnan@HMCTS.NET>
2021-02-18 11:42:25 +0000

    Updated adding judge email and phone scenario

--- AdminWebsite/AdminWebsite.AcceptanceTests/Pages/AssignJudgePage.cs
+++ AdminWebsite/AdminWebsite.AcceptanceTests/Pages/AssignJudgePage.cs
@@ -10,5 +10,7 @@ namespace AdminWebsite.AcceptanceTests.Pages
         public static By JudgePhoneTextField = By.Id("judgePhoneFld");
         public static By NextButton = By.Id("nextButton");
         public static By CancelButton = By.Id("cancelButton");
+        public static By JudgeEmailId = By.Id("judge-email");
+        public static By JudgePhoneId = By.Id("judge-phone");
     }
 }
