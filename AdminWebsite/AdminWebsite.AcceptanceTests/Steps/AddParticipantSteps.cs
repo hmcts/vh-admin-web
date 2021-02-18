0d31ec75f9474dcd260cc6dca5e5b3519b09b46a
Phillip Chambers <mail@phillipchambers.com>
2021-02-18 15:44:10 +0000

    test: update test to check for Case Role Name

--- AdminWebsite/AdminWebsite.AcceptanceTests/Steps/AddParticipantSteps.cs
+++ AdminWebsite/AdminWebsite.AcceptanceTests/Steps/AddParticipantSteps.cs
@@ -256,7 +256,7 @@ namespace AdminWebsite.AcceptanceTests.Steps
                 if (participant.Role.ToLower().Equals("judge") || participant.Role.ToLower().Equals("judge")) continue;
 
                 var fullNameTitle = $"{title} {participant.Firstname} {participant.Lastname}";
-                var expectedParticipant = $"{fullNameTitle} {participant.HearingRoleName}";
+                var expectedParticipant = $"{fullNameTitle} {participant.CaseRoleName}";
 
                 if (participant.HearingRoleName == PartyRole.Representative.Name)
                 {
