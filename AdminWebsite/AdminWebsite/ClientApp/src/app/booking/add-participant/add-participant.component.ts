dda1d8713aa536ca0457bfdbc41b04abd949ca67
Lionel Roche <lionel.roche@hmcts.net>
2021-02-24 17:09:05 +0000

    - fix to display interpreter in drop down in edit mode.

--- AdminWebsite/AdminWebsite/ClientApp/src/app/booking/add-participant/add-participant.component.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/booking/add-participant/add-participant.component.ts
@@ -107,9 +107,9 @@ export class AddParticipantComponent extends BookingBaseComponent implements OnI
         this.$subscriptions.push(
             this.participantsListComponent.selectedParticipant.subscribe(participantEmail => {
                 this.selectedParticipantEmail = participantEmail;
-                this.interpreterSelected = this.hearing.participants.some(
+                /*                 this.interpreterSelected = this.hearing.participants.some(
                     p => p.email === this.selectedParticipantEmail && p.hearing_role_name.toLowerCase() === HearingRoles.INTERPRETER
-                );
+                ); */
                 this.showDetails = true;
                 setTimeout(() => {
                     this.repopulateParticipantToEdit();
@@ -262,6 +262,7 @@ export class AddParticipantComponent extends BookingBaseComponent implements OnI
 
     private repopulateParticipantToEdit() {
         const selectedParticipant = this.hearing.participants.find(s => s.email === this.selectedParticipantEmail);
+        this.interpreterSelected = selectedParticipant.hearing_role_name.toLowerCase() === HearingRoles.INTERPRETER;
         if (selectedParticipant) {
             this.logger.debug(`${this.loggerPrefix} Repopulating participant to edit.`, {
                 hearing: this.hearing.hearing_id,
@@ -934,9 +935,9 @@ export class AddParticipantComponent extends BookingBaseComponent implements OnI
     private updateHearingRoleList(hearingRoleList: string[]) {
         // hide the interpreter value if participant list is empty or participant list has an interpreter.
         if (this.hearingHasAnInterpreter() || !this.hearingHasInterpretees()) {
-            // if (!this.interpreterSelected) {
-            this.hearingRoleList = this.hearingRoleList.filter(item => item.toLowerCase() !== HearingRoles.INTERPRETER);
-            // }
+            if (!this.interpreterSelected) {
+                this.hearingRoleList = this.hearingRoleList.filter(item => item.toLowerCase() !== HearingRoles.INTERPRETER);
+            }
         }
     }
     private removeInterpreteeAndInterpreter() {
