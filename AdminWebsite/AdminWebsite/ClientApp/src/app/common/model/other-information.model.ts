c488cf91429435dea74e888cb18c0cba1056c5cb
David Decent <david@squidbox.co.uk>
2021-03-12 15:25:03 +0000

    VIH-7436 removed tocamel method on otherinfo

--- AdminWebsite/AdminWebsite/ClientApp/src/app/common/model/other-information.model.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/common/model/other-information.model.ts
@@ -17,16 +17,4 @@ export class OtherInformationModel {
         }
         return otherInfo;
     }
-
-    static toCamel(objJson) {
-        const newObject = {};
-        for (const origKey in objJson) {
-            if (objJson.hasOwnProperty(origKey)) {
-                const newKey = (origKey.charAt(0).toUpperCase() + origKey.slice(1) || origKey).toString();
-                newObject[newKey] = objJson[origKey];
-            }
-        }
-
-        return newObject;
-    }
 }
