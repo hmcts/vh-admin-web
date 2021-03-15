6f65b43cb8f67545973965cd75744de4bc76a45b
David Decent <david@squidbox.co.uk>
2021-03-15 10:29:50 +0000

    VIH-7436 added more tests for pipe stringifier

--- AdminWebsite/AdminWebsite/ClientApp/src/app/services/pipe-stringifier.service.spec.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/services/pipe-stringifier.service.spec.ts
@@ -6,6 +6,14 @@ describe('PipeStringifierService', () => {
     let service: PipeStringifierService;
     const testString = '|name|test|height|100';
     const testObject = { name: 'test', height: '100' };
+    const testEmptyString = '|name||height|100';
+    const testEmptyObject = { height: '100' };
+    const testThreePropsString = '|name|test|height|100';
+    const testThreePropsObject = { name: 'test', height: '100' };
+    const testMissingValueString = '|name|height|100';
+    const testMissingValueObject = { name: 'height' };
+    const testPreserveCaseString = '|nAmE|tEst|height|100';
+    const testPreserveCaseObject = { nAmE: 'tEst', height: '100' };
     beforeEach(() => {
         TestBed.configureTestingModule({});
         service = TestBed.inject(PipeStringifierService);
@@ -21,5 +29,25 @@ describe('PipeStringifierService', () => {
 
     it('should decode a string into an object', () => {
         expect(service.decode(testString)).toEqual(testObject);
+    });
+
+    it('should decode a string with an empty value into an object', () => {
+        expect(service.decode(testEmptyString)).toEqual(testEmptyObject);
+    });
+
+    it('should decode a string with three values into an object', () => {
+        expect(service.decode(testThreePropsString)).toEqual(testThreePropsObject);
+    });
+
+    it('should decode a string with a missing value into an object', () => {
+        expect(service.decode(testMissingValueString)).toEqual(testMissingValueObject);
+    });
+
+    it('should decode a string and preserve case', () => {
+        expect(service.decode(testPreserveCaseString)).toEqual(testPreserveCaseObject);
+    });
+
+    it('should encode an object to a string and preserve case', () => {
+        expect(service.encode(testPreserveCaseObject)).toEqual(testPreserveCaseString);
     });
 });
