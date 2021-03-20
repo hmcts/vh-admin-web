7dafc151af451344b31d3268235d5ca69de9b5df
Viren Sareen <viren.sareen@solirius.com>
2021-03-20 13:54:33 +0000

    Updated booking details to show judge phone and email

--- AdminWebsite/AdminWebsite/ClientApp/src/app/bookings-list/participant-details/participant-details.component.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/bookings-list/participant-details/participant-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { ParticipantDetailsModel } from '../../common/model/participant-details.model';
+import { BookingsDetailsModel } from '../../common/model/bookings-list.model';
+import { OtherInformationModel } from '../../common/model/other-information.model';
 
 @Component({
     selector: 'app-booking-participant-details',
@@ -9,9 +11,24 @@ import { ParticipantDetailsModel } from '../../common/model/participant-details.
 export class ParticipantDetailsComponent {
     @Input()
     participant: ParticipantDetailsModel = null;
-
+    @Input()
+    hearing: BookingsDetailsModel;
     @Input()
     vh_officer_admin: boolean;
 
     constructor() {}
+
+    getJudgeEmail() {
+        const email = OtherInformationModel.init(this.hearing.OtherInformation).JudgeEmail;
+        return email;
+    }
+
+    getJudgePhone() {
+        const phone = OtherInformationModel.init(this.hearing.OtherInformation).JudgePhone;
+        return phone;
+    }
+
+    isJudge(participant: ParticipantDetailsModel) {
+        return participant.isJudge;
+    }
 }
