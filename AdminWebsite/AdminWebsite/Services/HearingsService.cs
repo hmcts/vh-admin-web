ea4859fcf8d1534ff4c53c07c4f771bd7f915eac
Viren Sareen <viren.sareen@solirius.com>
2021-03-24 10:43:02 +0000

    Fixed bug amendment before confirm notify

--- AdminWebsite/AdminWebsite/Services/HearingsService.cs
+++ AdminWebsite/AdminWebsite/Services/HearingsService.cs
@@ -155,12 +155,17 @@ namespace AdminWebsite.Services
                     .Where(x => !x.User_role_name.Contains("Judge", StringComparison.CurrentCultureIgnoreCase))
                     .ToList();
             }
-            var requests = participantsToEmail
-                .Select(participant =>
-                    AddNotificationRequestMapper.MapToHearingAmendmentNotification(updatedHearing, participant,
-                        caseName, caseNumber, originalHearing.Scheduled_date_time, updatedHearing.Scheduled_date_time))
-                .ToList();
 
+            var requests = new List<AddNotificationRequest>();
+            if (originalHearing.Confirmed_date != null)
+            {
+                requests = participantsToEmail
+                    .Select(participant =>
+                        AddNotificationRequestMapper.MapToHearingAmendmentNotification(updatedHearing, participant,
+                            caseName, caseNumber, originalHearing.Scheduled_date_time, updatedHearing.Scheduled_date_time))
+                    .ToList();
+            }
+            
             foreach (var request in requests)
             {
                 await _notificationApiClient.CreateNewNotificationAsync(request);
