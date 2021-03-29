58588fadcac84936cec4ffda9a1e4dc1494cf22a
Shaed Parkar <shaed.parkar@slimsolutions.company>
2021-03-29 13:02:17 +0100

    VIH-7534 Use TimeZoneConverter package to work cross platform

--- AdminWebsite/AdminWebsite/Extensions/DateExtensions.cs
+++ AdminWebsite/AdminWebsite/Extensions/DateExtensions.cs
@@ -1,19 +1,20 @@
 using System;
 using System.Globalization;
+using TimeZoneConverter;
 
 namespace AdminWebsite.Extensions
 {
     public static class DateExtensions
     {
-        private static readonly TimeZoneInfo BritishZone = TimeZoneInfo.FindSystemTimeZoneById("Europe/London");
+        private static readonly TimeZoneInfo BritishZone = TZConvert.GetTimeZoneInfo("Europe/London");
         public static string ToEmailDateGbLocale(this DateTime datetime)
         {
-            return TimeZoneInfo.ConvertTime(datetime, BritishZone).ToString("d MMMM yyyy", new CultureInfo("en-GB"));
+            return TimeZoneInfo.ConvertTimeFromUtc(datetime, BritishZone).ToString("d MMMM yyyy", new CultureInfo("en-GB"));
         }
         
         public static string ToEmailTimeGbLocale(this DateTime datetime)
         {
-            return TimeZoneInfo.ConvertTime(datetime, BritishZone).ToString("h:mm tt", new CultureInfo("en-GB"))
+            return TimeZoneInfo.ConvertTimeFromUtc(datetime, BritishZone).ToString("h:mm tt", new CultureInfo("en-GB"))
                 .ToUpper();
         }
     }
