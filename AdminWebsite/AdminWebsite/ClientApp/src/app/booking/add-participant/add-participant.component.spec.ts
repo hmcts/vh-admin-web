dbde8fc21aa7e9a93a41eed1ee52efd1ce6afd64
Jordan Mulvaney <Jordan.Mulvaney@version1.com>
2021-04-08 09:30:20 +0100

    Running lint & prettier

--- AdminWebsite/AdminWebsite/ClientApp/src/app/booking/add-participant/add-participant.component.spec.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/booking/add-participant/add-participant.component.spec.ts
@@ -767,8 +767,7 @@ describe('AddParticipantComponent', () => {
                 expect(searchServiceSpy.searchJudiciaryEntries).toHaveBeenCalledWith(email);
             });
 
-            it('should have errorJudiciaryAccount set to false if search service returns null and role is not Panel Member or Winger',
-            () => {
+            it('should have errorJudiciaryAccount set to false if search service returns null and role is not Panel Member or Winger', () => {
                 searchServiceSpy.searchJudiciaryEntries.and.returnValue(of(null));
                 component.validateJudiciaryEmailAndRole();
                 expect(component.errorJudiciaryAccount).toBeFalsy();
