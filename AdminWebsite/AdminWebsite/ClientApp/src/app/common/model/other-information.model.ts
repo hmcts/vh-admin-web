dae43e219f47054f62dc98be0f24add96ca3a20c
FarronF <farron.flaherty@hmcts.net>
2021-05-05 15:33:02 +0100

    Only encode other changes if there is a value. (#695)

--- AdminWebsite/AdminWebsite/ClientApp/src/app/common/model/other-information.model.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/common/model/other-information.model.ts
@@ -6,10 +6,19 @@ export class OtherInformationModel {
     OtherInformation?: string;
 
     static init(otherInformation: string): OtherInformationModel {
-        let otherInfo: OtherInformationModel = {};
+        let otherInfo: OtherInformationModel = new OtherInformationModel();
         const stringifier = new PipeStringifierService();
         try {
             otherInfo = stringifier.decode(otherInformation);
+            if (!otherInfo.JudgeEmail) {
+                otherInfo.JudgeEmail = null;
+            }
+            if (!otherInfo.JudgePhone) {
+                otherInfo.JudgePhone = null;
+            }
+            if (!otherInfo.OtherInformation) {
+                otherInfo.OtherInformation = null;
+            }
         } catch (error) {
             otherInfo.JudgeEmail = null;
             otherInfo.JudgePhone = null;
