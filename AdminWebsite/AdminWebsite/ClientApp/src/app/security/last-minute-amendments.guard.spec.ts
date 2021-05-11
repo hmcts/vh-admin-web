367f64345427bacaa6f5b08967b99c3eba98bf11
FarronHmcts <farron.flaherty@hmcts.net>
2021-05-11 16:11:32 +0100

    Adding LastMinuteGuard tests

--- AdminWebsite/AdminWebsite/ClientApp/src/app/security/last-minute-amendments.guard.spec.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/security/last-minute-amendments.guard.spec.ts
@@ -3,37 +3,63 @@ import { AuthGuard } from './auth.guard';
 import { Router } from '@angular/router';
 import { MockOidcSecurityService } from '../testing/mocks/MockOidcSecurityService';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { LastMinuteAmendmentsGuard } from './last-minute-amendments.guard';
+import { VideoHearingsService } from '../services/video-hearings.service';
 
-describe('authguard', () => {
-    let authGuard: AuthGuard;
-    let oidcSecurityService;
+describe('LastMinuteAmendmentsGuard', () => {
+    let guard: LastMinuteAmendmentsGuard;
     const router = {
         navigate: jasmine.createSpy('navigate')
     };
 
+    const videoHearingsServiceSpy = jasmine.createSpyObj<VideoHearingsService>([
+        'isConferenceClosed',
+        'isHearingAboutToStart'
+    ]);
+    const redirectPath = '/summary';
+
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [
-                AuthGuard,
-                { provide: OidcSecurityService, useClass: MockOidcSecurityService },
+                LastMinuteAmendmentsGuard,
+                { provide: VideoHearingsService, useValue: videoHearingsServiceSpy },
                 { provide: Router, useValue: router }
             ]
         }).compileComponents();
-        oidcSecurityService = TestBed.inject(OidcSecurityService);
-        authGuard = TestBed.inject(AuthGuard);
+        guard = TestBed.inject(LastMinuteAmendmentsGuard);
+
+        videoHearingsServiceSpy.isConferenceClosed.and.returnValue(false);
+        videoHearingsServiceSpy.isHearingAboutToStart.and.returnValue(true);
     });
 
-    describe('when logged in with successful authentication', () => {
-        it('canActivate should return true', () => {
-            oidcSecurityService.setAuthenticated(true);
-            authGuard.canActivate().subscribe(result => expect(result).toBeTruthy());
+    describe('when can activate', () => {
+        let returned: boolean;
+        it('should return true when not about to start', () => {
+            videoHearingsServiceSpy.isHearingAboutToStart.and.returnValue(false);
+        });
+
+        it('should return true when conference is closed', () => {
+            videoHearingsServiceSpy.isConferenceClosed.and.returnValue(true);
+        });
+
+        it('should return true when conference is closed and about to start', () => {
+            videoHearingsServiceSpy.isConferenceClosed.and.returnValue(true);
+            videoHearingsServiceSpy.isHearingAboutToStart.and.returnValue(false);
+        });
+
+        afterEach(() => {
+            returned = guard.canActivate();
+            expect(returned).toBe(true);
+            expect(router.navigate).not.toHaveBeenCalled();
         });
     });
 
-    describe('when login failed with unsuccessful authentication', () => {
-        it('canActivate should return false', fakeAsync(() => {
-            oidcSecurityService.setAuthenticated(false);
-            authGuard.canActivate().subscribe(result => expect(result).toBeFalsy());
-        }));
+    describe('when cannot activate', () => {
+        it('canActivate should return true', () => {
+            const returned = guard.canActivate();
+            expect(returned).toBe(false);
+            expect(router.navigate).toHaveBeenCalledWith([redirectPath]);
+            expect(router.navigate).toHaveBeenCalledTimes(1);
+        });
     });
 });
