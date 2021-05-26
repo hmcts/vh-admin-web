7bb8daa53ef5ef47ea081157229fd2d71d6af4a0
robert bennett <robertbennett1998@outlook.com>
2021-05-26 15:11:18 +0100

    Removed

--- AdminWebsite/AdminWebsite/ClientApp/src/app/get-audio-file/get-audio-file-request-timedout-warning/get-audio-file-request-timedout-warning.component.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/get-audio-file/get-audio-file-request-timedout-warning/get-audio-file-request-timedout-warning.component.ts
@@ -3,10 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-get-audio-file-request-timedout-warning',
     templateUrl: './get-audio-file-request-timedout-warning.component.html',
-    styles: ['']
 })
-export class GetAudioFileRequestTimedoutWarningComponent implements OnInit {
-    constructor() {}
-
-    ngOnInit(): void {}
+export class GetAudioFileRequestTimedoutWarningComponent{
 }
