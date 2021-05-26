98abae9cf1faad726477fe1ea7b33ca4a60858cf
Jordan Mulvaney <Jordan.Mulvaney@version1.com>
2021-05-26 16:23:33 +0100

    Updated method & test

--- AdminWebsite/AdminWebsite/ClientApp/src/app/booking/participant/item/participant-item.component.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/booking/participant/item/participant-item.component.ts
@@ -95,7 +95,7 @@ export class ParticipantItemComponent {
     }
 
     canEditJudge(): boolean {
-        if ((!this.canEdit || this.videoHearingsService.isConferenceClosed()) && this.videoHearingsService.isHearingAboutToStart()) {
+        if (!this.canEdit || this.videoHearingsService.isConferenceClosed() || this.videoHearingsService.isHearingAboutToStart()) {
             return false;
         }
         return true;
