11669d0f2dc62cf6bc9d64638d6d79c5507c1a2b
Robert Bennett <robertbennett1998@outlook.com>
2021-05-27 09:56:07 +0100

    Ran lint and prettier

--- AdminWebsite/AdminWebsite/ClientApp/src/app/get-audio-file/get-audio-file-request-timedout-warning/get-audio-file-request-timedout-warning.component.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/get-audio-file/get-audio-file-request-timedout-warning/get-audio-file-request-timedout-warning.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-get-audio-file-request-timedout-warning',
-    templateUrl: './get-audio-file-request-timedout-warning.component.html',
+    templateUrl: './get-audio-file-request-timedout-warning.component.html'
 })
-export class GetAudioFileRequestTimedoutWarningComponent{
-}
+export class GetAudioFileRequestTimedoutWarningComponent {}
