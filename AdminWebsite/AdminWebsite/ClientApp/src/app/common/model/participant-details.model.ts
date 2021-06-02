e8bbc566a57270d20c20a44b0faf852d8e834a70
alberto-hortiguela <alberto.hortiguela@hmcts.net>
2021-06-02 15:34:10 +0100

    Null - undefined value for title, firstname and lastname

--- AdminWebsite/AdminWebsite/ClientApp/src/app/common/model/participant-details.model.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/common/model/participant-details.model.ts
@@ -21,9 +21,9 @@ export class ParticipantDetailsModel {
         isInterpretee: boolean
     ) {
         this.ParticipantId = participantId;
-        this.FirstName = firstName;
-        this.LastName = lastName;
-        this.Title = title === undefined ? '' : title;
+        this.FirstName = firstName === null ? '' : firstName;
+        this.LastName = lastName === null ? '' : lastName;
+        this.Title = title === null ? '' : title;
         this.UserRoleName = role;
         this.UserName = userName;
         this.Flag = false;
