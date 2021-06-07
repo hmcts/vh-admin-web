6628108d4fbc8cbb8dcc471324980d12428a7e5a
robert bennett <robertbennett1998@outlook.com>
2021-06-07 09:39:57 +0100

    VIH-7635 display error message when audio search times out (#713)
    * Refactored the audio-link service and get-audio-file component to catch 504 errors. Updated relevant tests and added required new tests.
    
    * Refactored audio search forms into their own components.
    
    * Moved the result views into their respective parent folders.
    
    * Added warning for a 504 error when searching cvp audio files.
    
    * Ran prettier and linter.
    
    * Removed fdescribes
    
    * Added timedout warning for vh audio file search
    
    * Improved the way errors are caught.
    
    * Added code to remove previous search results immediatly when a new one is made. Updated tests.
    
    * Tided up error handling and updated unit tests
    
    * Removed unused CSS files.
    
    * Removed style sheet links on components for removed style sheets
    
    * VIH-7012 multi date selection (#714)
    
    * Removed
    
    * Ran lint and prettier
    
    * Loading spinner CVP audio search
    
    Co-authored-by: Robert Bennett <Robert.Bennett@version1.com>
    Co-authored-by: Phillip Chambers <phillipjchambers@gmail.com>
    Co-authored-by: alberto-hortiguela <alberto.hortiguela@hmcts.net>
    Co-authored-by: alberto-hortiguela <80695759+alberto-hortiguela@users.noreply.github.com>

---
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/get-audio-file/get-audio-file-vh/get-audio-file-vh.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { HearingAudioSearchModel } from 'src/app/common/model/hearing-audio-search-model';
+import { AudioLinkService, IVhAudioRecordingResult } from 'src/app/services/audio-link-service';
+import { Logger } from 'src/app/services/logger';
+
+@Component({
+    selector: 'app-get-audio-file-vh',
+    templateUrl: './get-audio-file-vh.component.html'
+})
+export class GetAudioFileVhComponent implements OnInit {
+    private readonly loggerPrefix = '[GetAudioFileVh] -';
+    vhAudioFileForm: FormGroup;
+    searchResult: IVhAudioRecordingResult;
+    get results(): HearingAudioSearchModel[] {
+        return !this.searchResult?.result ? [] : (this.searchResult?.result).map(x => new HearingAudioSearchModel(x));
+    }
+    today = new Date();
+
+    constructor(private fb: FormBuilder, private audioLinkService: AudioLinkService, private logger: Logger) {}
+
+    async ngOnInit(): Promise<void> {
+        this.logger.debug(`${this.loggerPrefix} Landed on get audio file`);
+
+        this.vhAudioFileForm = this.fb.group({
+            caseNumber: [null],
+            vhDate: [null]
+        });
+    }
+
+    get caseNumber() {
+        return this.vhAudioFileForm.get('caseNumber');
+    }
+
+    get vhDate() {
+        return this.vhAudioFileForm.get('vhDate');
+    }
+
+    get vhSearchCriteriaSet(): boolean {
+        return this.caseNumber.value || this.vhDate.value;
+    }
+
+    get vhDateInvalid() {
+        const todayDate = new Date(new Date());
+        return new Date(this.vhDate.value) > todayDate && (this.vhDate.dirty || this.vhDate.touched);
+    }
+
+    async search() {
+        if (this.searchResult) {
+            this.searchResult = null;
+        }
+
+        this.logger.debug(`${this.loggerPrefix} Attempting to search for audio recording`);
+        if (this.vhSearchCriteriaSet) {
+            const date: Date = this.vhDate.value ? new Date(this.vhDate.value) : undefined;
+            const caseNumber: string = this.caseNumber.value ? this.caseNumber.value : undefined;
+
+            this.logger.debug(`${this.loggerPrefix} Getting results by case number/date`, { caseNumber, date });
+            this.searchResult = await this.audioLinkService.searchForHearingsByCaseNumberOrDate(caseNumber, date);
+
+            if (this.searchResult.error) {
+                this.logger.error(
+                    `${this.loggerPrefix} Error retrieving vh audio file link for: ${date} and ${caseNumber}`,
+                    this.searchResult.error
+                );
+            }
+        }
+    }
+}
