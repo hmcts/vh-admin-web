4929ef3ae7b1bdea595d7f3630d52f7c2d4ecbfe
Jordan Mulvaney <Jordan.Mulvaney@version1.com>
2021-06-15 15:33:47 +0100

    Readded tests but changed expectations

--- AdminWebsite/AdminWebsite/ClientApp/src/app/booking/add-participant/add-participant.component.spec.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/booking/add-participant/add-participant.component.spec.ts
@@ -756,10 +756,16 @@ describe('AddParticipantComponent', () => {
             const testCases = [
                 { searchJudiciaryEntriesValue: null, role: '', expectError: false },
                 { searchJudiciaryEntriesValue: null, role: 'Other', expectError: false },
+                { searchJudiciaryEntriesValue: null, role: 'Panel Member', expectError: false },
+                { searchJudiciaryEntriesValue: null, role: 'Winger', expectError: false },
                 { searchJudiciaryEntriesValue: emptyPersonResponse, role: '', expectError: false },
                 { searchJudiciaryEntriesValue: emptyPersonResponse, role: 'Other', expectError: false },
+                { searchJudiciaryEntriesValue: emptyPersonResponse, role: 'Panel Member', expectError: false },
+                { searchJudiciaryEntriesValue: emptyPersonResponse, role: 'Winger', expectError: false },
                 { searchJudiciaryEntriesValue: populatedPersonResponse, role: '', expectError: true },
-                { searchJudiciaryEntriesValue: populatedPersonResponse, role: 'Other', expectError: true }
+                { searchJudiciaryEntriesValue: populatedPersonResponse, role: 'Other', expectError: true },
+                { searchJudiciaryEntriesValue: populatedPersonResponse, role: 'Panel Member', expectError: true },
+                { searchJudiciaryEntriesValue: populatedPersonResponse, role: 'Winger', expectError: true }
             ];
 
             beforeEach(
