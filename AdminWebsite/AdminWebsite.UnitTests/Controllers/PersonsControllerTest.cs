e2188dfd8c3e60f1c2690154f0a4bfb7a160ec2a
robert bennett <robertbennett1998@outlook.com>
2021-07-07 14:28:42 +0100

    VIH-7932 Judge accounts available for selection for individual participants (#755)
    * Added logic to filter judge participants. Updated unit tests
    
    * Tidied the where statement for filtering judges
    
    * Made suggested changes

--- AdminWebsite/AdminWebsite.UnitTests/Controllers/PersonsControllerTest.cs
+++ AdminWebsite/AdminWebsite.UnitTests/Controllers/PersonsControllerTest.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Text.Encodings.Web;
 using AdminWebsite.UnitTests.Helper;
 using AdminWebsite.Configuration;
+using AdminWebsite.Contracts.Responses;
 using AdminWebsite.Services;
 using BookingsApi.Client;
 using BookingsApi.Contract.Requests;
@@ -55,35 +56,183 @@ namespace AdminWebsite.UnitTests.Controllers
         }
 
         [Test]
-        public async Task Should_return_request_if_match_to_search_term()
+        public async Task Should_return_searched_participants()
         {
-            _response.Add(new PersonResponse
-                            {
-                                Id = Guid.NewGuid(),
-                                ContactEmail = "jackman@hmcts.net1",
-                                FirstName = "Jack",
-                                LastName = "Mann",
-                                TelephoneNumber = "111222333",
-                                Title = "Mr",
-                                MiddleNames = "No",
-                                Username = "jackman@hmcts.net"
-            });
-            _bookingsApiClient.Setup(x => x.PostPersonBySearchTermAsync(It.IsAny<SearchTermRequest>()))
-                              .ReturnsAsync(_response);
-
+            // Arrange
+            var additionalParticipantToReturn = new PersonResponse
+            {
+                Id = Guid.NewGuid(),
+                ContactEmail = "jackfilter@hmcts.net",
+                FirstName = "Filter",
+                LastName = "Participant",
+                TelephoneNumber = "111222333",
+                Title = "Mr",
+                MiddleNames = "No",
+                Username = "jackfilter@hmcts.net"
+            };
+            _response.Add(additionalParticipantToReturn);
+            
             var searchTerm = "ado";
+            
+            var expectedResponse = new List<PersonResponse>();
+            expectedResponse.Add(_response[0]);
+            expectedResponse.Add(additionalParticipantToReturn);
+            
+            _bookingsApiClient.Setup(x => x.PostPersonBySearchTermAsync(It.Is<SearchTermRequest>(searchTermRequest => searchTermRequest.Term == searchTerm)))
+                .ReturnsAsync(_response);
+
+            _userAccountService.Setup(x => x.GetJudgeUsers()).ReturnsAsync(new List<JudgeResponse>());
+
+            _bookingsApiClient.Setup(x => x.PostJudiciaryPersonBySearchTermAsync(It.Is<SearchTermRequest>(searchTermRequest => searchTermRequest.Term == searchTerm)))
+                .ReturnsAsync(new List<PersonResponse>());
+
+            // Act
             var result = await _controller.PostPersonBySearchTerm(searchTerm);
 
-            var okRequestResult = (OkObjectResult)result.Result;
-            okRequestResult.StatusCode.Should().NotBeNull();
-            var personRespList = (List<PersonResponse>)okRequestResult.Value;
-            personRespList.Count.Should().Be(1);
-            personRespList[0].ContactEmail.Should().Be(_response[0].ContactEmail);
+            // Assert
+            var okObjectResult = result.Result.Should().BeAssignableTo<OkObjectResult>().Which;
+            okObjectResult.Value.Should().BeEquivalentTo(expectedResponse);
+            _bookingsApiClient.Verify(x => x.PostPersonBySearchTermAsync(It.Is<SearchTermRequest>(request => request.Term == searchTerm)), Times.Once);
+            _bookingsApiClient.Verify(x => x.PostJudiciaryPersonBySearchTermAsync(It.Is<SearchTermRequest>(request => request.Term == searchTerm)), Times.Once);
+            _userAccountService.Verify(x => x.GetJudgeUsers(), Times.Once);
+        }
+        
+        [Test]
+        public async Task Should_filter_judge_participants()
+        {
+            // Arrange
+            var participantToFilter = new PersonResponse
+            {
+                Id = Guid.NewGuid(),
+                ContactEmail = "jackfilter@hmcts.net",
+                FirstName = "Filter",
+                LastName = "Participant",
+                TelephoneNumber = "111222333",
+                Title = "Mr",
+                MiddleNames = "No",
+                Username = "jackfilter@hmcts.net"
+            };
+            _response.Add(participantToFilter);
+
+            var searchTerm = "ado";
+            
+            var expectedResponse = new List<PersonResponse>();
+            expectedResponse.Add(_response[0]);
+            
+            _bookingsApiClient.Setup(x => x.PostPersonBySearchTermAsync(It.Is<SearchTermRequest>(searchTermRequest => searchTermRequest.Term == searchTerm)))
+                .ReturnsAsync(_response);
+
+            _userAccountService.Setup(x => x.GetJudgeUsers()).ReturnsAsync(new[]
+            {
+                new JudgeResponse()
+                {
+                    Email = participantToFilter.ContactEmail
+                }
+            });
+
+            _bookingsApiClient.Setup(x => x.PostJudiciaryPersonBySearchTermAsync(It.Is<SearchTermRequest>(searchTermRequest => searchTermRequest.Term == searchTerm)))
+                .ReturnsAsync(new List<PersonResponse>());
+            // Act
+            var result = await _controller.PostPersonBySearchTerm(searchTerm);
+
+            // Assert
+            var okObjectResult = result.Result.Should().BeAssignableTo<OkObjectResult>().Which;
+            okObjectResult.Value.Should().BeEquivalentTo(expectedResponse);
+            _bookingsApiClient.Verify(x => x.PostPersonBySearchTermAsync(It.Is<SearchTermRequest>(request => request.Term == searchTerm)), Times.Once);
+            _bookingsApiClient.Verify(x => x.PostJudiciaryPersonBySearchTermAsync(It.Is<SearchTermRequest>(request => request.Term == searchTerm)), Times.Once);
+            _userAccountService.Verify(x => x.GetJudgeUsers(), Times.Once);
         }
 
         [Test]
+        public async Task Should_filter_judiciary_participants()
+        {
+            // Arrange
+            var participantToFilter = new PersonResponse
+            {
+                Id = Guid.NewGuid(),
+                ContactEmail = "jackfilter@hmcts.net",
+                FirstName = "Filter",
+                LastName = "Participant",
+                TelephoneNumber = "111222333",
+                Title = "Mr",
+                MiddleNames = "No",
+                Username = "jackfilter@hmcts.net"
+            };
+            _response.Add(participantToFilter);
+            
+            var searchTerm = "ado";
+            
+            var expectedResponse = new List<PersonResponse>();
+            expectedResponse.Add(_response[0]);
+            
+            _bookingsApiClient.Setup(x => x.PostPersonBySearchTermAsync(It.Is<SearchTermRequest>(searchTermRequest => searchTermRequest.Term == searchTerm)))
+                .ReturnsAsync(_response);
+
+            _userAccountService.Setup(x => x.GetJudgeUsers()).ReturnsAsync(new List<JudgeResponse>());
+
+            _bookingsApiClient.Setup(x => x.PostJudiciaryPersonBySearchTermAsync(It.Is<SearchTermRequest>(searchTermRequest => searchTermRequest.Term == searchTerm)))
+                .ReturnsAsync(new[] {participantToFilter});
+
+            // Act
+            var result = await _controller.PostPersonBySearchTerm(searchTerm);
+
+            // Assert
+            var okObjectResult = result.Result.Should().BeAssignableTo<OkObjectResult>().Which;
+            okObjectResult.Value.Should().BeEquivalentTo(expectedResponse);
+            _bookingsApiClient.Verify(x => x.PostPersonBySearchTermAsync(It.Is<SearchTermRequest>(request => request.Term == searchTerm)), Times.Once);
+            _bookingsApiClient.Verify(x => x.PostJudiciaryPersonBySearchTermAsync(It.Is<SearchTermRequest>(request => request.Term == searchTerm)), Times.Once);
+            _userAccountService.Verify(x => x.GetJudgeUsers(), Times.Once);
+        }
+        
+        [Test]
+        public async Task Should_filter_TestUsernameStem()
+        {
+            // Arrange
+            var participantToFilter = new PersonResponse
+            {
+                Id = Guid.NewGuid(),
+                ContactEmail = "jackfilter@hmcts.net1",
+                FirstName = "Filter",
+                LastName = "Participant",
+                TelephoneNumber = "111222333",
+                Title = "Mr",
+                MiddleNames = "No",
+                Username = "jackfilter@hmcts.net"
+            };
+            _response.Add(participantToFilter);
+            
+            var searchTerm = "ado";
+            
+            var expectedResponse = new List<PersonResponse>();
+            expectedResponse.Add(_response[0]);
+            
+            _bookingsApiClient.Setup(x => x.PostPersonBySearchTermAsync(It.Is<SearchTermRequest>(searchTermRequest => searchTermRequest.Term == searchTerm)))
+                .ReturnsAsync(_response);
+
+            _userAccountService.Setup(x => x.GetJudgeUsers()).ReturnsAsync(new List<JudgeResponse>());
+
+            _bookingsApiClient.Setup(x => x.PostJudiciaryPersonBySearchTermAsync(It.Is<SearchTermRequest>(searchTermRequest => searchTermRequest.Term == searchTerm)))
+                .ReturnsAsync(new List<PersonResponse>());
+
+            // Act
+            var result = await _controller.PostPersonBySearchTerm(searchTerm);
+
+            // Assert
+            var okObjectResult = result.Result.Should().BeAssignableTo<OkObjectResult>().Which;
+            okObjectResult.Value.Should().BeEquivalentTo(expectedResponse);
+            _bookingsApiClient.Verify(x => x.PostPersonBySearchTermAsync(It.Is<SearchTermRequest>(request => request.Term == searchTerm)), Times.Once);
+            _bookingsApiClient.Verify(x => x.PostJudiciaryPersonBySearchTermAsync(It.Is<SearchTermRequest>(request => request.Term == searchTerm)), Times.Once);
+            _userAccountService.Verify(x => x.GetJudgeUsers(), Times.Once);
+        }
+        
+        [Test]
         public async Task Should_pass_on_bad_request_from_bookings_api()
         {
+            _userAccountService.Setup(x => x.GetJudgeUsers()).ReturnsAsync(new List<JudgeResponse>());
+
+            _bookingsApiClient.Setup(x => x.PostJudiciaryPersonBySearchTermAsync(It.IsAny<SearchTermRequest>()))
+                .ReturnsAsync(new List<PersonResponse>());
+            
             _bookingsApiClient.Setup(x => x.PostPersonBySearchTermAsync(It.IsAny<SearchTermRequest>()))
                   .ThrowsAsync(ClientException.ForBookingsAPI(HttpStatusCode.BadRequest));
 
@@ -94,6 +243,11 @@ namespace AdminWebsite.UnitTests.Controllers
         [Test]
         public void Should_pass_on_exception_request_from_bookings_api()
         {
+            _userAccountService.Setup(x => x.GetJudgeUsers()).ReturnsAsync(new List<JudgeResponse>());
+
+            _bookingsApiClient.Setup(x => x.PostJudiciaryPersonBySearchTermAsync(It.IsAny<SearchTermRequest>()))
+                .ReturnsAsync(new List<PersonResponse>());
+            
             _bookingsApiClient.Setup(x => x.PostPersonBySearchTermAsync(It.IsAny<SearchTermRequest>()))
                   .ThrowsAsync(ClientException.ForBookingsAPI(HttpStatusCode.InternalServerError));
             Assert.ThrowsAsync<BookingsApiException>(() => _controller.PostPersonBySearchTerm("term"));
