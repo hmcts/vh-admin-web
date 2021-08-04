2b9c500da338faf1af7b1f9b85a163abd586be65
ibthmcts <ibrahim.tholley@HMCTS.NET>
2021-08-04 14:10:02 +0100

    Revert "VIH-8068 Use en-GB date format on saucelabs"
    This reverts commit 27a623c68e74ff0091068a3d4318958abaab7669.

--- AdminWebsite/AdminWebsite.AcceptanceTests/Data/DateFormats.cs
+++ AdminWebsite/AdminWebsite.AcceptanceTests/Data/DateFormats.cs
@@ -6,7 +6,7 @@ namespace AdminWebsite.AcceptanceTests.Data
     {
         public static string LocalDateFormat(bool runningOnSaucelabs)
         {
-            if (runningOnSaucelabs) return new CultureInfo("en-GB").DateTimeFormat.ShortDatePattern;
+            if (runningOnSaucelabs) return new CultureInfo("es-PR").DateTimeFormat.ShortDatePattern;
             return CultureInfo.CurrentCulture.Name.ToLower().Equals("en-us") || CultureInfo.CurrentCulture.TwoLetterISOLanguageName.ToLower().Equals("iv")
                 ? new CultureInfo("en-GB").DateTimeFormat.ShortDatePattern
                 : CultureInfo.CurrentUICulture.DateTimeFormat.ShortDatePattern;
