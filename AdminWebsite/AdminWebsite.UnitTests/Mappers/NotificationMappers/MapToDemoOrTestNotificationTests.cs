890fc8f3f49399ce3db8abca16f8be23ac0166ba
Prasanna Dommalapati <prasanna.dommalapati@gmail.com>
2021-08-11 15:05:10 +0100

    VIH-7798 Staff Member notification mappings (#788)
    * VIH-7798 Staff Member notification mappings
    
    * VIH-7798 Update notifications nuget package

--- AdminWebsite/AdminWebsite.UnitTests/Mappers/NotificationMappers/MapToDemoOrTestNotificationTests.cs
+++ AdminWebsite/AdminWebsite.UnitTests/Mappers/NotificationMappers/MapToDemoOrTestNotificationTests.cs
@@ -114,7 +114,62 @@ namespace AdminWebsite.UnitTests.Mappers.NotificationMappers
             result.MessageType.Should().Be(MessageType.Email);
             result.PhoneNumber.Should().Be(participant.TelephoneNumber);
             result.Parameters.Should().BeEquivalentTo(expectedParameters);
+        }        
+        
+        [Test]
+        public void Should_map_staffmember_demo_or_test_notification()
+        {
+            //Arrange
+            var hearing = new HearingDetailsResponse
+            {
+                Id = Guid.NewGuid(),
+                Participants = new List<ParticipantResponse>(),
+                ScheduledDateTime = new DateTime(2020, 2, 10, 12, 15, 0, DateTimeKind.Utc)
+            };
+            const NotificationType expectedNotificationType = NotificationType.StaffMemberDemoOrTest;
+            const string testType = "Generic";
+            const string caseNumber = "MBFY/17364";
+
+            var participant = new ParticipantResponse
+            {
+                Id = Guid.NewGuid(),
+                Username = "contact@hmcts.net",
+                CaseRoleName = "caserolename",
+                ContactEmail = "contact@hearings.reform.hmcts.net",
+                FirstName = "John",
+                HearingRoleName = "hearingrolename",
+                LastName = "Doe",
+                TelephoneNumber = "0123456789",
+                UserRoleName = "Staff Member",
+                DisplayName = "Johnny",
+            };
+
+            hearing.Participants.Add(participant);
+
+            var expectedParameters = new Dictionary<string, string>
+            {
+                {"case number", caseNumber},
+                {"test type", "Generic"},
+                {"date", "10 February 2020"},
+                {"time", "12:15 PM"},
+                {"staff member", $"{participant.FirstName} {participant.LastName}"},
+                {"username", participant.Username}
+            };
+
+            //Act
+            var result = AddNotificationRequestMapper.MapToDemoOrTestNotification(hearing, participant, caseNumber, testType);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.HearingId.Should().Be(hearing.Id);
+            result.ParticipantId.Should().Be(participant.Id);
+            result.ContactEmail.Should().Be(participant.ContactEmail);
+            result.NotificationType.Should().Be(expectedNotificationType);
+            result.MessageType.Should().Be(MessageType.Email);
+            result.PhoneNumber.Should().Be(participant.TelephoneNumber);
+            result.Parameters.Should().BeEquivalentTo(expectedParameters);
         }
+
         [Test]
         public void Should_map_ejud_joh_demo_or_test_notification()
         {
@@ -168,7 +223,7 @@ namespace AdminWebsite.UnitTests.Mappers.NotificationMappers
             result.PhoneNumber.Should().Be(participant.TelephoneNumber);
             result.Parameters.Should().BeEquivalentTo(expectedParameters);
         }
-        
+
         [Test]
         public void Should_map_participants_demo_or_test_notification()
         {
