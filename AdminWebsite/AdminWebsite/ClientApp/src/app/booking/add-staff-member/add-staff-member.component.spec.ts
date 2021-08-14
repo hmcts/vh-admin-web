a460f2c9d789c3937b9776bfc89cff1baa3083c6
Arif Ahmed <arifahmed2009@hotmail.co.uk>
2021-08-14 20:58:31 +0100

    Staff member is now emitted from component when it's form is valid

--- AdminWebsite/AdminWebsite/ClientApp/src/app/booking/add-staff-member/add-staff-member.component.spec.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/booking/add-staff-member/add-staff-member.component.spec.ts
@@ -1,16 +1,18 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
+import { MockComponent } from 'ng-mocks';
+import { Subscription } from 'rxjs';
 import { BookingService } from 'src/app/services/booking.service';
 import { Logger } from 'src/app/services/logger';
 import { VideoHearingsService } from 'src/app/services/video-hearings.service';
 import { provide } from 'src/app/testing/helpers/jasmine.helpers';
 import { AddParticipantBaseDirective } from '../add-participant-base/add-participant-base.component';
+import { SearchEmailComponent } from '../search-email/search-email.component';
 
 import { AddStaffMemberComponent } from './add-staff-member.component';
 
-describe('AddStaffMemberComponent', () => {
+fdescribe('AddStaffMemberComponent', () => {
   let component: AddStaffMemberComponent;
   let fixture: ComponentFixture<AddStaffMemberComponent>;
 
@@ -19,6 +21,7 @@ describe('AddStaffMemberComponent', () => {
   let routerSpy: jasmine.SpyObj<Router>;
   let loggerSpy: jasmine.SpyObj<Logger>;
 
+  const staffMemberRole = 'StaffMember';
   
   beforeEach(
     waitForAsync(() => {
@@ -28,7 +31,8 @@ describe('AddStaffMemberComponent', () => {
       loggerSpy = jasmine.createSpyObj('Logger', ['']);
 
       TestBed.configureTestingModule({
-        declarations: [AddStaffMemberComponent],
+        imports: [FormsModule, ReactiveFormsModule],
+        declarations: [AddStaffMemberComponent, MockComponent(SearchEmailComponent)],
         providers: [
           provide(BookingService, bookingServiceSpy),
           provide(Logger, loggerSpy),
@@ -53,6 +57,110 @@ describe('AddStaffMemberComponent', () => {
       component.ngOnInit();
 
       expect(component.initialiseForm).toHaveBeenCalledTimes(1);
+    });
+    
+    it('should setup event emissions', () => {
+      component.setupStaffMemberValidityEmissionOnFormValueChange = spyOn(component, 'setupStaffMemberValidityEmissionOnFormValueChange');
+      component.setupStaffMemberEmissionWhenValid = spyOn(component, 'setupStaffMemberEmissionWhenValid');
+
+      component.ngOnInit();
+
+      expect(component.setupStaffMemberValidityEmissionOnFormValueChange).toHaveBeenCalledTimes(1);
+      expect(component.setupStaffMemberEmissionWhenValid).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('initialiseForm', () => {
+    it('should call super method implementation', () => {
+      const initialiseFormSpy = spyOn(AddParticipantBaseDirective.prototype, 'initialiseForm');
+
+      component.initialiseForm();
+
+      expect(initialiseFormSpy).toHaveBeenCalledTimes(1);
+    });
+    
+    it('should set role value', () => {
+      component.initialiseForm();
+
+      expect(component.role.value).toBe(staffMemberRole);
+    });
+    
+    it('should set party value', () => {
+      component.initialiseForm();
+
+      expect(component.party.value).toBe(staffMemberRole);
+    });
+  });
+      
+  describe('setupStaffMemberValidityEmissionOnFormValueChange', () => {
+    it('should subscribe to email changes when search email exists', fakeAsync((done) => {
+      const email = 'email@email.com';
+      component.isSubscribedToEmailChanges = false;
+
+      tick();
+      component.searchEmail.emailChanged.subscribe((value) => {
+        expect(value).toBe(email);
+        done();
+      });
+
+      component.setupStaffMemberValidityEmissionOnFormValueChange();
+
+      component.role.setValue('newRole');
+    }));
+
+    it('should emit if staff member is valid on form change', (done) => {
+      component.isStaffMemberValid.subscribe((valid) => {
+        expect(valid).not.toBeNull();
+        done();
+      });
+
+      component.setupStaffMemberValidityEmissionOnFormValueChange();
+
+      component.role.setValue('newRole');
+    });
+
+    it('should add subscriptions to subscriptions array', () => {
+      component.$subscriptions = [];
+      component.isSubscribedToEmailChanges = false;
+
+      component.setupStaffMemberValidityEmissionOnFormValueChange();
+      component.role.setValue('newRole');
+
+      expect(component.$subscriptions.length).toBe(2);
+    });
+  });
+
+  describe('setupStaffMemberEmissionWhenValid', () => {
+    it('should add staff member validity to subscriptions array', () => {
+      component.$subscriptions = [];
+
+      component.setupStaffMemberEmissionWhenValid();
+
+      expect(component.$subscriptions.length).toBe(1);
+    });
+    
+    it('should emit staff member if valid', (done) => {
+      component.staffMember.subscribe((staffMember) => {
+        expect(staffMember).not.toBeNull();
+        done();
+      });
+
+      component.setupStaffMemberEmissionWhenValid();
+
+      component.isStaffMemberValid.emit(true);
+    });
+  });
+
+  describe('ngOnDestroy', () => {
+    it('should unsubscribe all subcriptions on destroy component', () => {
+      component.$subscriptions.push(new Subscription(), new Subscription());
+      expect(component.$subscriptions[0].closed).toBeFalsy();
+      expect(component.$subscriptions[1].closed).toBeFalsy();
+
+      component.ngOnDestroy();
+
+      expect(component.$subscriptions[0].closed).toBeTruthy();
+      expect(component.$subscriptions[1].closed).toBeTruthy();
     });
   });
 });
--- AdminWebsite/AdminWebsite/ClientApp/src/app/booking/add-staff-member/add-staff-member.component.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/booking/add-staff-member/add-staff-member.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { ParticipantModel } from 'src/app/common/model/participant.model';
 import { AddParticipantBaseDirective } from 'src/app/booking/add-participant-base/add-participant-base.component'
 import { Router } from '@angular/router';
 import { BookingService } from 'src/app/services/booking.service';
 import { Logger } from 'src/app/services/logger';
 import { VideoHearingsService } from 'src/app/services/video-hearings.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-staff-member',
   templateUrl: './add-staff-member.component.html',
   styleUrls: ['./add-staff-member.component.css']
 })
-export class AddStaffMemberComponent extends AddParticipantBaseDirective implements OnInit {
-  private staffMember: ParticipantModel;
+  
+export class AddStaffMemberComponent extends AddParticipantBaseDirective implements OnInit, OnDestroy {
+  @Output() staffMember = new EventEmitter<ParticipantModel>();
+  @Output() isStaffMemberValid = new EventEmitter<boolean>();
+
+  $subscriptions: Subscription[] = [];
+
+  private readonly staffMemberRole = 'StaffMember';
+
+  isSubscribedToEmailChanges = false;
 
   constructor(
     protected bookingService: BookingService,
@@ -20,14 +29,65 @@ export class AddStaffMemberComponent extends AddParticipantBaseDirective impleme
     protected videoHearingService: VideoHearingsService,
     protected logger: Logger) {
       super(bookingService, router, videoHearingService, logger);
-    }
+  }
 
   ngOnInit(): void {
-    this.staffMember = new ParticipantModel();
     this.initialiseForm();
+
+    this.setupStaffMemberValidityEmissionOnFormValueChange();
+    this.setupStaffMemberEmissionWhenValid();
+  }
+
+  ngOnDestroy(): void {
+    this.$subscriptions.forEach(subscription => {
+        if (subscription) {
+            subscription.unsubscribe();
+        }
+    });
+  }
+
+  initialiseForm() {
+    super.initialiseForm();
+    this.form.removeControl('interpreterFor');
+
+    this.role.setValue(this.staffMemberRole);
+    this.party.setValue(this.staffMemberRole);
   }
   
-  saveStaffMember(): void {
-    console.log(this.staffMember);
+  setupStaffMemberValidityEmissionOnFormValueChange(): void {
+    const formValueChangeSubscription = this.form.valueChanges.subscribe(() => {
+      if (!this.isSubscribedToEmailChanges && this.searchEmail) {
+        this.isSubscribedToEmailChanges = true;
+        const emailChangeSubscription = this.searchEmail.emailChanged.subscribe(email => this.email.setValue(email));
+        
+        this.$subscriptions.push(emailChangeSubscription);
+      }
+
+      this.isStaffMemberValid.emit(this.form.valid)
+    });
+
+    this.$subscriptions.push(formValueChangeSubscription);
+  }
+  
+  setupStaffMemberEmissionWhenValid(): void {
+    const isStaffMemberValidSubscription = this.isStaffMemberValid.subscribe(isValid => {
+      if (!isValid)
+        return;
+
+      const staffMember = this.mapFormToStaffMember();
+      this.staffMember.emit(staffMember);
+    });
+
+    this.$subscriptions.push(isStaffMemberValidSubscription);
+  }
+
+  private mapFormToStaffMember() {
+    return new ParticipantModel({
+      display_name: this.displayName.value,
+      first_name: this.firstName.value,
+      last_name: this.lastName.value,
+      phone: this.phone.value,
+      email: this.email.value
+    })
   }
 }
