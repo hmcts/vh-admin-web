8c06f49a796270cae59c0b1b6a2fa8b05935e41a
Robert Bennett <robertbennett1998@outlook.com>
2021-09-01 09:46:59 +0100

    Fixed bad logic for detecting when participant list has changed

--- AdminWebsite/AdminWebsite/ClientApp/src/app/booking/participant/list/participant-list.component.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/booking/participant/list/participant-list.component.ts
@@ -26,8 +26,8 @@ export class ParticipantListComponent implements OnInit, OnChanges, DoCheck {
     constructor(private logger: Logger, private videoHearingsService: VideoHearingsService) {}
 
     ngDoCheck(): void {
-        const containsNewParticipants = this.hearing.participants?.some(hearingParticipant => this.sortedParticipants.includes(hearingParticipant)) ?? false;
-        const containsRemovedParticipants = this.sortedParticipants?.some(sortedParticipant => !this.hearing.participants.includes(sortedParticipant)) ?? false;
+        const containsNewParticipants = !this.hearing?.participants?.every(hearingParticipant => this.sortedParticipants.includes(hearingParticipant)) ?? false;
+        const containsRemovedParticipants = !this.sortedParticipants?.every(sortedParticipant => this.hearing.participants.includes(sortedParticipant)) ?? false;
 
         if (containsNewParticipants || containsRemovedParticipants) {
             this.sortParticipants();
@@ -58,7 +58,7 @@ export class ParticipantListComponent implements OnInit, OnChanges, DoCheck {
         return this.$selectedForRemove;
     }
 
-    private sortParticipants() {
+    sortParticipants() {
         if (!this.hearing.participants) {
             this.sortedParticipants = [];
             return;
