5d5edf29f9cf791602a4d588362eb95dcc7cf21a
Prasanna Dommalapati <prasanna.dommalapati@gmail.com>
2022-02-15 17:50:55 +0000

    VIH-8629 judge email optional for notification

--- AdminWebsite/AdminWebsite.UnitTests/Services/HearingServiceTests.cs
+++ AdminWebsite/AdminWebsite.UnitTests/Services/HearingServiceTests.cs
@@ -947,7 +947,7 @@ namespace AdminWebsite.UnitTests.Services
         }
 
         [Test]
-        public async Task NewHearingSendConfirmation_Should_Receive_Call_For_JUDGE_And_STAFFMEMBER()
+        public async Task NewHearingSendConfirmation_Should_Receive_Call_For_JUDGE_And_NOT_STAFFMEMBER()
         {
             await _service.NewHearingSendConfirmation(_hearing);
 
@@ -1213,6 +1213,7 @@ namespace AdminWebsite.UnitTests.Services
             var judge = Builder<ParticipantResponse>.CreateNew()
                 .With(x => x.Id = Guid.NewGuid())
                 .With(x => x.UserRoleName = "Judge")
+                .With(x => x.ContactEmail = "Judge{@court.com")
                 .Build();
             var staffMember = Builder<ParticipantResponse>.CreateNew()
                 .With(x => x.Id = Guid.NewGuid())
