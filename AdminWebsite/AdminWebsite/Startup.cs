3a5742a125859b5d83254565314d1d28b8e6aa78
Christopher Pateman <christopher.pateman@version1.com>
2022-08-11 15:28:36 +0100

    VIH-9236 - Correct Secrets

--- AdminWebsite/AdminWebsite/Startup.cs
+++ AdminWebsite/AdminWebsite/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Logging;
+using System.Collections.Generic;
 
 namespace AdminWebsite
 {
