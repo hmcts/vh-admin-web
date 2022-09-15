77baea8d26a368494553a86eea799d75a340e75f
Arif Ahmed <arifahmed2009@hotmail.co.uk>
2022-09-15 17:30:48 +0100

    Added error message for file size for working hours file upload

--- AdminWebsite/AdminWebsite/ClientApp/src/app/work-allocation/work-allocation.component.spec.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/work-allocation/work-allocation.component.spec.ts
@@ -51,9 +51,43 @@ Allocate hearings`);
 
             expect(componentOuterDiv.innerText).toEqual('Edit working hours / non-availability');
         });
+
+        it('should show working hours file upload max size error', () => {
+            component.isWorkingHoursFileUploadError = true;
+            component.workingHoursFileUploadError = 'error message';
+            fixture.detectChanges();
+
+            const error = fixture.debugElement.query(By.css('#working-hours-file-upload-error'))
+                .nativeElement.innerText;
+            expect(error).toContain('Error: error message');
+        });
     });
 
     it('should retrieve vh team leader status', () => {
         expect(component.isVhTeamLeader).toBeTruthy();
     });
+
+    describe('handleFileInput', () => {
+        it('should reset working hour file upload errors', () => {
+            component.isWorkingHoursFileUploadError = true;
+            component.workingHoursFileUploadError = 'error message';
+            
+            const file = new File([""], "filename", { type: 'text/html' });
+
+            component.handleFileInput(file);
+
+            expect(component.isWorkingHoursFileUploadError).toBe(false);
+            expect(component.workingHoursFileUploadError).toBe('');
+        });
+        
+        it('should set errors when maximum file size is exceeded', () => {
+            const file = new File([""], "filename", { type: 'text/html' });
+            Object.defineProperty(file, 'size', { value: 2000001 });
+
+            component.handleFileInput(file);
+
+            expect(component.isWorkingHoursFileUploadError).toBe(true);
+            expect(component.workingHoursFileUploadError).toBe('File cannot be larger than 200kb');
+        });
+      });
 });
--- AdminWebsite/AdminWebsite/ClientApp/src/app/work-allocation/work-allocation.component.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/work-allocation/work-allocation.component.ts
@@ -8,11 +8,30 @@ import { UserIdentityService } from '../services/user-identity.service';
     styleUrls: ['./work-allocation.component.scss']
 })
 export class WorkAllocationComponent {
+    public isWorkingHoursFileUploadError = false;
     public isVhTeamLeader = false;
+
+    public workingHoursFileUploadError = '';
+
+    public workingHoursFile: File | null = null;
+
+    maxFileUploadSize = 200000;
 
     constructor(private userIdentityService: UserIdentityService) {
         this.userIdentityService.getUserInformation().subscribe((userProfileResponse: UserProfileResponse) => {
             this.isVhTeamLeader = userProfileResponse.is_vh_team_leader;
         });
+    }
+
+    handleFileInput(file: File) {
+        this.isWorkingHoursFileUploadError = false;
+        this.workingHoursFileUploadError = '';
+ 
+        this.workingHoursFile = file;
+
+        if (this.workingHoursFile.size > this.maxFileUploadSize) {
+            this.isWorkingHoursFileUploadError = true;
+            this.workingHoursFileUploadError = `File cannot be larger than ${this.maxFileUploadSize / 1000}kb`;
+         };
     }
 }
