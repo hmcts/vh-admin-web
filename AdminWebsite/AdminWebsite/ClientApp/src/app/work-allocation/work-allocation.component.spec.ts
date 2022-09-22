3ad807a5be42d92ec49cf2888ff47b35e535d8ec
Arif Ahmed <arifahmed2009@hotmail.co.uk>
2022-09-22 17:51:40 +0100

    Added validation for csv file and tested some of the code

--- AdminWebsite/AdminWebsite/ClientApp/src/app/work-allocation/work-allocation.component.spec.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/work-allocation/work-allocation.component.spec.ts
@@ -6,7 +6,7 @@ import { UserIdentityService } from '../services/user-identity.service';
 
 import { WorkAllocationComponent } from './work-allocation.component';
 
-describe('WorkAllocationComponent', () => {
+fdescribe('WorkAllocationComponent', () => {
     let component: WorkAllocationComponent;
     let fixture: ComponentFixture<WorkAllocationComponent>;
 
@@ -54,7 +54,7 @@ Allocate hearings`);
 
         it('should show working hours file upload max size error', () => {
             component.isWorkingHoursFileUploadError = true;
-            component.workingHoursFileUploadError = 'error message';
+            component.workingHoursFileUploadErrors.push('error message');
             fixture.detectChanges();
 
             const error = fixture.debugElement.query(By.css('#working-hours-file-upload-error'))
@@ -68,16 +68,13 @@ Allocate hearings`);
     });
 
     describe('handleFileInput', () => {
-        it('should reset working hour file upload errors', () => {
-            component.isWorkingHoursFileUploadError = true;
-            component.workingHoursFileUploadError = 'error message';
-            
+        it('should reset file upload errors', () => {
+            const resetErrorsSpy = spyOn(component, 'resetErrors');
             const file = new File([""], "filename", { type: 'text/html' });
 
             component.handleFileInput(file);
 
-            expect(component.isWorkingHoursFileUploadError).toBe(false);
-            expect(component.workingHoursFileUploadError).toBe('');
+            expect(resetErrorsSpy).toHaveBeenCalled();
         });
         
         it('should set errors when maximum file size is exceeded', () => {
@@ -87,7 +84,119 @@ Allocate hearings`);
             component.handleFileInput(file);
 
             expect(component.isWorkingHoursFileUploadError).toBe(true);
-            expect(component.workingHoursFileUploadError).toBe('File cannot be larger than 200kb');
+            expect(component.workingHoursFileUploadErrors[0]).toBe('File cannot be larger than 200kb');
+        });
+    });
+
+    describe('resetErrors', () => {
+        it('should reset errors', () => {
+            component.isWorkingHoursFileUploadError = true;
+            component.workingHoursFileUploadErrors.push('error message');
+        
+            component.resetErrors();
+
+            expect(component.isWorkingHoursFileUploadError).toBe(false);
+            expect(component.workingHoursFileUploadErrors).toEqual([]);
+        });
+    });
+
+    describe('uploadWorkingHours', () => {
+        it('should reset file upload errors', () => {
+            const resetErrorsSpy = spyOn(component, 'resetErrors');
+
+            component.uploadWorkingHours();
+
+            expect(resetErrorsSpy).toHaveBeenCalled();
+        });
+        
+        it('should does not read file if file does not exist', () => {
+            spyOn(component, 'resetErrors');
+            const readFileSpy = spyOn(component, 'readFile');
+
+            component.uploadWorkingHours();
+
+            expect(readFileSpy).not.toHaveBeenCalled();
+        });
+        
+        it('should read file', () => {
+            spyOn(component, 'resetErrors');
+            const readFileSpy = spyOn(component, 'readFile');
+
+            component.workingHoursFile = new File([""], "filename", { type: 'text/html' });
+
+            component.uploadWorkingHours();
+
+            expect(readFileSpy).toHaveBeenCalledWith(component.workingHoursFile);
+        });
+    });
+
+    describe('validateTimeCell', () => {
+        const testCases = [
+            { case: 'hour is not a number', timeCell: [NaN, 30], errorMessage: 'Value is not a valid time' },
+            { case: 'minute is not a number', timeCell: [9, NaN], errorMessage: 'Value is not a valid time' },
+            { case: 'hour is before work hours', timeCell: [8, 30], errorMessage: 'Hour value (8) is not within 08:00 - 18:00' },
+            { case: 'hour is after work hours', timeCell: [19, 30], errorMessage: 'Hour value (19) is not within 08:00 - 18:00' },
+            { case: 'minute is above 59', timeCell: [9, -1], errorMessage: 'Minutes value (-1) is not within 0-59' },
+            { case: 'minute is below 0', timeCell: [9, 60], errorMessage: 'Minutes value (60) is not within 0-59' },
+        ];
+        
+        testCases.forEach(testCase => {
+            it(`should return false and add error message when ${testCase.case}`, () => {
+                const isValid = component.validateTimeCell(testCase.timeCell, '');
+
+                expect(isValid).toBeFalsy();
+                expect(component.workingHoursFileUploadErrors[0])
+                    .toBe(testCase.errorMessage);
+            })
+        });
+        
+        it('should add space to passed in error message', () => {
+            component.validateTimeCell([NaN, 30], 'Location -');
+            expect(component.workingHoursFileUploadErrors[0]).toBe('Location - Value is not a valid time');
+        });
+
+        it('should return true with no error message', () => {
+            const isValid = component.validateTimeCell([9, 30], 'Location -');
+
+            expect(isValid).toBeTruthy();
+            expect(component.workingHoursFileUploadErrors[0]).toBeUndefined();
+        });
+    });
+    
+    describe('validateTimeCell', () => {
+        const testCases = [
+            { startTimeHour: 11, startTimeMinute: 0, endTimeHour: 9, endTimeMinute: 0},
+            { startTimeHour: 11, startTimeMinute: 0, endTimeHour: 9, endTimeMinute: 30},
+            { startTimeHour: 12, startTimeMinute: 30, endTimeHour: 12, endTimeMinute: 20},
+        ];
+        
+        testCases.forEach(testCase => {
+            it(`should return false and add error message when end time is before start time`, () => {
+                const startTime = `${testCase.startTimeHour}:${testCase.startTimeMinute == 0 ? '00' : testCase.startTimeMinute}`;
+                const endTime = `${testCase.endTimeHour}:${testCase.endTimeMinute == 0 ? '00' : testCase.endTimeMinute}`;
+                
+                const isValid = component.validateStartTimeBeforeEndTime(
+                    testCase.startTimeHour, testCase.startTimeMinute, testCase.endTimeHour, testCase.endTimeMinute, '');
+
+                expect(isValid).toBeFalsy();
+                expect(component.workingHoursFileUploadErrors[0])
+                    .toBe(`End time ${endTime} is before start time ${startTime}`);
+            })
+        });
+        
+        it('should add space to passed in error message', () => {
+            const startTime = '11:00'
+            const endTime = '9:00'
+            
+            component.validateStartTimeBeforeEndTime(11, 0, 9, 0, 'Location -');
+            expect(component.workingHoursFileUploadErrors[0]).toBe(`Location - End time ${endTime} is before start time ${startTime}`);
+        });
+
+        it('should return true with no error message', () => {
+            const isValid = component.validateStartTimeBeforeEndTime(9, 0, 17, 0, 'Location -');
+
+            expect(isValid).toBeTruthy();
+            expect(component.workingHoursFileUploadErrors[0]).toBeUndefined();
         });
     });
 });
--- AdminWebsite/AdminWebsite/ClientApp/src/app/work-allocation/work-allocation.component.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/work-allocation/work-allocation.component.ts
@@ -3,6 +3,7 @@ import { DayWorkingHours } from '../common/model/day-working-hours'
 import { WorkAvailability } from '../common/model/work-availability'
 import { UserProfileResponse } from '../services/clients/api-client';
 import { UserIdentityService } from '../services/user-identity.service';
+import { convertToNumberArray } from '../common/helpers/array-helpers';
 
 @Component({
     selector: 'app-work-allocation',
@@ -14,7 +15,7 @@ export class WorkAllocationComponent {
     public isWorkingHoursFileUploadError = false;
     public isVhTeamLeader = false;
 
-    public workingHoursFileUploadError = '';
+    public workingHoursFileUploadErrors: string[] = [];
 
     public workingHoursFile: File | null = null;
 
@@ -27,72 +28,171 @@ export class WorkAllocationComponent {
     }
 
     handleFileInput(file: File) {
-        this.isWorkingHoursFileUploadError = false;
-        this.workingHoursFileUploadError = '';
- 
+        this.resetErrors();
+
+        if (!file) {
+            return;
+        }
+
         this.workingHoursFile = file;
 
         if (this.workingHoursFile.size > this.maxFileUploadSize) {
             this.isWorkingHoursFileUploadError = true;
-            this.workingHoursFileUploadError = `File cannot be larger than ${this.maxFileUploadSize / 1000}kb`;
+            this.workingHoursFileUploadErrors.push(`File cannot be larger than ${this.maxFileUploadSize / 1000}kb`);
             return;
         };
     }
 
+    resetErrors() {
+        this.isWorkingHoursFileUploadError = false;
+        this.workingHoursFileUploadErrors = [];
+    }
+
     //  TODO: Test
+    readFile(file: File) {
+        const reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = (e) => this.readWorkAvailability(e.target.result as string);
+    }
+
+    //  TODO: Test
+    readWorkAvailability(text: string): any {
+        const delimiter = ',';
+        const timeDelimiter = ':';
+
+        let userWorkAvailabilityRows = text.split("\n");
+        // Remove headings rows
+        userWorkAvailabilityRows.splice(0, 2);
+
+        const workAvailabilities: WorkAvailability[] = [];
+
+        userWorkAvailabilityRows.forEach((row, index) => {
+            const values = row.split(delimiter);
+
+            const workAvailability = new WorkAvailability();
+            workAvailability.username = values[0];
+
+            const workingHours: DayWorkingHours[] = [];
+
+            let dayOfWeekId = 1;
+
+            for (let i = 1; i < values.length; i++) {
+                let dayWorkingHours = new DayWorkingHours();
+
+                const rowNumber = index + 3;
+                const entryNumber = i + 1;
+                
+                const startTimeArray = convertToNumberArray(values[i].split(timeDelimiter));
+                const endTimeArray = convertToNumberArray(values[i + 1].split(timeDelimiter));
+
+                let isDelimiterError = false;
+                [startTimeArray, endTimeArray].forEach((array, arrayIndex) => {
+                    if (array.length !== 2) {
+                        this.isWorkingHoursFileUploadError = isDelimiterError = true;
+                        this.workingHoursFileUploadErrors.push(`Row ${rowNumber}, Entry: ${entryNumber + arrayIndex} - Incorrect delimiter used. Please use a colon to separate the hours and minutes.`);
+                    }
+                    return;
+                })
+
+                if (isDelimiterError) {
+                    i++;
+                    continue;
+                }
+                
+                const isStartTimeCellValid =
+                    this.validateTimeCell(startTimeArray, `Row ${rowNumber}, Entry: ${entryNumber} -`);
+                const isEndTimeCellValid =
+                    this.validateTimeCell(endTimeArray, `Row ${rowNumber}, Entry: ${entryNumber + 1} -`);
+                
+                const isStartTimeBeforeEndTime = this.validateStartTimeBeforeEndTime(
+                    startTimeArray[0],
+                    startTimeArray[1],
+                    endTimeArray[0],
+                    endTimeArray[1],
+                    `Row ${rowNumber}, Entry: ${entryNumber}-${entryNumber + 1} -`
+                );
+                                
+                if (!this.isWorkingHoursFileUploadError) {
+                    this.isWorkingHoursFileUploadError =
+                        !isStartTimeCellValid || !isEndTimeCellValid || !isStartTimeBeforeEndTime;
+                }
+                
+                if (!this.isWorkingHoursFileUploadError) {
+                    dayWorkingHours.dayOfWeekId = dayOfWeekId++;
+                    dayWorkingHours.startTimeHour = startTimeArray[0];
+                    dayWorkingHours.startTimeMinutes = startTimeArray[1];
+                    dayWorkingHours.endTimeHour = endTimeArray[0];
+                    dayWorkingHours.endTimeMinutes = endTimeArray[1];
+                    workingHours.push(dayWorkingHours);
+                }
+
+                i++
+            }
+
+            workAvailability.workingHours = workingHours;
+            workAvailabilities.push(workAvailability);
+        });
+    }
+
     uploadWorkingHours() {
+        this.resetErrors();
+
         if (!this.workingHoursFile) {
             return;
         }
 
-        this.readWorkAvailability(this.workingHoursFile);
+        this.readFile(this.workingHoursFile);
     }
 
-    //  TODO: Test
-    readWorkAvailability(file: File) {
-        const reader = new FileReader();
-        reader.readAsText(file);
-        
-        reader.onload = function (e) {
-            const text = e.target.result as string;
-            const delimiter = ',';
+    validateTimeCell(timeCell: number[], errorLocationMessaage: string = ''): boolean {
+        const hour = timeCell[0];
+        const minutes = timeCell[1];
 
-            let userWorkAvailabilityRows = text.split("\n");
-            // Remove headings rows
-            userWorkAvailabilityRows.splice(0, 2);
+        if (errorLocationMessaage) {
+            errorLocationMessaage = errorLocationMessaage + ' ';
+        }
 
-            const workAvailabilities: WorkAvailability[] = [];
+        let hasError = false;
+        if (isNaN(hour) || isNaN(minutes)) {
+            this.workingHoursFileUploadErrors.push(`${errorLocationMessaage}Value is not a valid time`);
+            hasError = true;
+        } else {
+            if (hour < 9 || hour > 17) {
+                this.workingHoursFileUploadErrors.push(`${errorLocationMessaage}Hour value (${hour}) is not within 08:00 - 18:00`);
+                hasError = true;
+            }
+            
+            if (minutes < 0 || minutes > 59) {
+                this.workingHoursFileUploadErrors.push(`${errorLocationMessaage}Minutes value (${minutes}) is not within 0-59`);
+                hasError = true;
+            }
+        }
 
-            userWorkAvailabilityRows.forEach((row, index) => {
-                const values = row.split(delimiter);
+        return !hasError;
+    }
 
-                const workAvailability = new WorkAvailability();
-                workAvailability.username = values[0];
+    validateStartTimeBeforeEndTime(
+        startTimeHour: number,
+        startTimeMinute: number,
+        endTimeHour: number,
+        endTimeMinute: number,
+        errorLocationMessaage: string = ''
+    ) {
+        if (errorLocationMessaage) {
+            errorLocationMessaage = errorLocationMessaage + ' ';
+        }
 
-                const workingHours: DayWorkingHours[] = [];
+        if ((endTimeHour < startTimeHour) ||
+            (endTimeHour === startTimeHour && endTimeMinute < startTimeMinute)) {
+            const startTime = `${startTimeHour}:${startTimeMinute == 0 ? '00' : startTimeMinute}`;
+            const endTime = `${endTimeHour}:${endTimeMinute == 0 ? '00' : endTimeMinute}`;
+            
+            this.workingHoursFileUploadErrors
+                .push(`${errorLocationMessaage}End time ${endTime} is before start time ${startTime}`);
 
-                let dayOfWeekId = 1;
+            return false; 
+        }
 
-                for (let i = 1; i < values.length; i++) {
-                    let dayWorkingHours = new DayWorkingHours();
-                    
-                    const startTimeArray = values[i].split(':');
-                    const endTimeArray = values[++i].split(':');
-                    
-                    dayWorkingHours.dayOfWeekId = dayOfWeekId++;
-                    dayWorkingHours.startTimeHour = parseInt(startTimeArray[0]);
-                    dayWorkingHours.startTimeMinutes = parseInt(startTimeArray[1]);
-                    dayWorkingHours.endTimeHour = parseInt(endTimeArray[0]);
-                    dayWorkingHours.endTimeMinutes = parseInt(endTimeArray[1]);
-                    workingHours.push(dayWorkingHours);
-                }
-    
-                workAvailability.workingHours = workingHours;
-                workAvailabilities.push(workAvailability);
-            });
-
-
-            console.log('Arif', workAvailabilities);
-        };
+        return true;
     }
 }
