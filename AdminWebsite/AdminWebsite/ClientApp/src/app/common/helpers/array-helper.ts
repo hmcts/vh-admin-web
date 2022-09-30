c10e48e95bfb3ea349288dab244a592841e4844e
AWildBugAppeared <50028681+AWildBugAppeared@users.noreply.github.com>
2022-09-30 16:45:42 +0100

    VIH-9234 Upload working hours file frontend (#969)
    * Added upload buttons
    
    * Added error message for file size for working hours file upload
    
    * Added method to read work availability
    
    * Added validation for csv file and tested some of the code
    
    * Refactoring and unit tests
    
    * Finished adding error handling
    
    * Refactor
    
    * Removed fdescribe
    
    * Sonarcloud fixes
    
    * Delinting
    
    * Fixed typo
    
    * Fixed typos
    
    * delinting

---
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/common/helpers/array-helper.ts
@@ -0,0 +1,9 @@
+export function convertToNumberArray(array: string[]) {
+    const numberArray: number[] = [];
+
+    array.forEach(element => {
+        numberArray.push(parseInt(element, 10));
+    });
+
+    return numberArray;
+}
