4e1fa1a5cc369c3f3a5ec5518a4c0cd07c7cdb98
Will Craig <william.craig2@justice.gov.uk>
2022-10-19 14:01:08 +0100

    Created edit-work-hours.component and test

--- AdminWebsite/AdminWebsite/ClientApp/src/app/work-allocation/edit-work-hours/edit-work-hours.component.spec.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/work-allocation/edit-work-hours/edit-work-hours.component.spec.ts
@@ -1,25 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditWorkHoursComponent } from './edit-work-hours.component';
+import {VhoSearchResponse} from "../../services/clients/api-client";
 
 describe('EditWorkHoursComponent', () => {
-  let component: EditWorkHoursComponent;
-  let fixture: ComponentFixture<EditWorkHoursComponent>;
+    let component: EditWorkHoursComponent;
+    let fixture: ComponentFixture<EditWorkHoursComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ EditWorkHoursComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [EditWorkHoursComponent]
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(EditWorkHoursComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(EditWorkHoursComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('setSearchResult should assign event to results property', () => {
+        const parameter = new VhoSearchResponse();
+        component.setSearchResult(parameter)
+        expect(component).toBeTruthy();
+        expect(component.result).toBe(parameter);
+    });
 });
--- AdminWebsite/AdminWebsite/ClientApp/src/app/work-allocation/edit-work-hours/edit-work-hours.component.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/work-allocation/edit-work-hours/edit-work-hours.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {VhoSearchResponse} from '../../services/clients/api-client';
 
 @Component({
-  selector: 'app-edit-work-hours',
-  templateUrl: './edit-work-hours.component.html',
-  styleUrls: ['./edit-work-hours.component.css']
+    selector: 'app-edit-work-hours',
+    templateUrl: './edit-work-hours.component.html',
+    styleUrls: ['./edit-work-hours.component.css']
 })
 export class EditWorkHoursComponent implements OnInit {
 
-  constructor() { }
+    result: VhoSearchResponse;
+    @Input() isVhTeamLeader: boolean;
 
-  ngOnInit(): void {
-  }
+    constructor() {}
 
+    ngOnInit(): void {}
+
+    setSearchResult($event: VhoSearchResponse) {
+        this.result = $event;
+    }
 }
