b9b000b70dc178e4b6e5cdb1f5a6f91c939a490a
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-01-16 22:31:27 +0000

    remove unused user action directive

--- AdminWebsite/AdminWebsite/ClientApp/src/app/shared/directives/user-action.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UserActionDirective } from './user-action.directive';
-
-describe('DisappearOnClickDirective', () => {
-    it('should create an instance', () => {
-        const directive = new UserActionDirective();
-        expect(directive).toBeTruthy();
-    });
-});
