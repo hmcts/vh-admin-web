64a4c6da9f31cfec1a40610666c420917bacd46a
Oliver Scott <oliver.scott@justice.gov.uk>
2023-02-07 17:53:45 +0000

    Lint and prettier

--- AdminWebsite/AdminWebsite/ClientApp/src/app/services/user-identity.service.spec.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/services/user-identity.service.spec.ts
@@ -34,37 +34,31 @@ describe('UserIdentityService', () => {
         });
     }));
 
-    it('should retrieve user profile from api and save to memory', inject(
-        [UserIdentityService],
-        (service: UserIdentityService) => {
-            const userProfile = new UserProfileResponse({
-                is_case_administrator: false,
-                is_vh_officer_administrator_role: true,
-                is_vh_team_leader: true
-            });
+    it('should retrieve user profile from api and save to memory', inject([UserIdentityService], (service: UserIdentityService) => {
+        const userProfile = new UserProfileResponse({
+            is_case_administrator: false,
+            is_vh_officer_administrator_role: true,
+            is_vh_team_leader: true
+        });
 
-            bhClientSpy.getUserProfile.and.returnValue(of(userProfile));
+        bhClientSpy.getUserProfile.and.returnValue(of(userProfile));
 
-            service.getUserInformation().subscribe(result => {
-                expect(result.is_case_administrator).toEqual(userProfile.is_case_administrator);
-                expect(result.is_vh_officer_administrator_role).toEqual(userProfile.is_vh_officer_administrator_role);
-                expect(result.is_vh_team_leader).toEqual(userProfile.is_vh_team_leader);
-            });
-        }
-    ));
+        service.getUserInformation().subscribe(result => {
+            expect(result.is_case_administrator).toEqual(userProfile.is_case_administrator);
+            expect(result.is_vh_officer_administrator_role).toEqual(userProfile.is_vh_officer_administrator_role);
+            expect(result.is_vh_team_leader).toEqual(userProfile.is_vh_team_leader);
+        });
+    }));
 
-    it('should clear set profile', inject(
-        [UserIdentityService],
-        (service: UserIdentityService) => {
-            const userProfile = new UserProfileResponse({
-                is_case_administrator: false,
-                is_vh_officer_administrator_role: true,
-                is_vh_team_leader: true
-            });
+    it('should clear set profile', inject([UserIdentityService], (service: UserIdentityService) => {
+        const userProfile = new UserProfileResponse({
+            is_case_administrator: false,
+            is_vh_officer_administrator_role: true,
+            is_vh_team_leader: true
+        });
 
-            service.profile = userProfile;
-            service.clearUserProfile();
-            expect(service.profile).toBeNull();
-        }
-    ));
+        service.profile = userProfile;
+        service.clearUserProfile();
+        expect(service.profile).toBeNull();
+    }));
 });
