427c89425463a3f6c936f620bdc24f5878b9db57
Phillip Chambers <phillipjchambers@gmail.com>
2023-03-14 10:02:14 +0000

    Remove unused import

--- AdminWebsite/AdminWebsite/ClientApp/src/app/services/justice-users.service.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/services/justice-users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { shareReplay } from 'rxjs/operators';
 import {
     AddJusticeUserRequest,
     BHClient,
