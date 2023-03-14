c725ed27eec2be9e5328ab73c5db67180ac5d02c
marcogagliardi <marco.gagliardi@justice.gov.uk>
2023-03-14 07:21:51 +0000

    code smell fix

--- AdminWebsite/AdminWebsite/ClientApp/src/app/work-allocation/upload-work-hours/upload-work-hours.component.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/work-allocation/upload-work-hours/upload-work-hours.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FileType } from '../../common/model/file-type';
 import { WorkHoursFileProcessorService } from '../services/work-hours-file-processor.service';
-import { error } from '@angular/compiler-cli/src/transformers/util';
 
 @Component({
     selector: 'app-upload-work-hours',
