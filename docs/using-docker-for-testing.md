912e123f1b3c7d7315c9b0adf3bf49392366d083
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-03-25 10:46:45 +0000

    remove sonar scanner from docker and stick to tasks

--- docs/using-docker-for-testing.md
+++ docs/using-docker-for-testing.md
@@ -3,17 +3,9 @@
 ## Create the admin web image locally
 
 ``` shell
-docker build . --file tests/Dockerfile -t admin-web-tests --build-arg PAT=$pat
+docker build . --file tests/Dockerfile -t admin-web-tests
 docker run --name admin-web-local --network=host -it --mount src="$(pwd)",target=/app,type=bind admin-web-tests:latest
 ```
-
-## Setup a local instance of Sonar
-
-``` shell
-docker run -d --name sonarqube -e SONAR_ES_BOOTSTRAP_CHECKS_DISABLE=true -p 9000:9000 sonarqube:latest
-```
-
-The unit and integration tests can be run inside a container. You will need a an access token to build the image locally
 
 ## Running all tests in Docker
 
@@ -22,8 +14,6 @@ Open a terminal at the root level of the repository and run the following comman
 ``` shell
 docker-compose -f "docker-compose.tests.yml" up --build --abort-on-container-exit
 ```
-
-> You may need to create a `.env` file to store the environment variables
 
 ## Convert test results into coverage report
 
--- scripts/tests/run-tests.sh
+++ scripts/tests/run-tests.sh
@@ -6,11 +6,9 @@ rm -d -r ${PWD}/TestResults
 rm -d -r ${PWD}/AdminWebsite/AdminWebsite/ClientApp/node_modules
 
 configuration=Release
-# dotnet sonarscanner begin /k:"${SONAR_PROJECT_KEY}" /o:"${SONAR_ORG}" /version:"${SONAR_PROJECT_VERSION}" /name:"${SONAR_PROJECT_NAME}" /d:sonar.host.url="${SONAR_HOST}" /d:sonar.login="${SONAR_TOKEN}" /d:sonar.pullrequest.key="${SONAR_PR_KEY}" /d:sonar.pullrequest.branch="${SONAR_PR_BRANCH}" /d:sonar.pullrequest.base="${SONAR_PR_BASE}" /s:"${PWD}/vh-sonar-settings.xml"
 
 exclusions="[Testing.Common]*,[AdminWebsite.Common]AdminWebsite.Common.*,[AdminWebsite]AdminWebsite.Security.*,[AdminWebsite]AdminWebsite.Configuration.*,[AdminWebsite]AdminWebsite.Pages.*,[AdminWebsite.Testing.Common]*"
 
-dotnet build AdminWebsite/AdminWebsite.sln -c $configuration
 # Script is for docker compose tests where the script is at the root level
 dotnet test AdminWebsite/AdminWebsite.UnitTests/AdminWebsite.UnitTests.csproj -c $configuration --no-build --results-directory ./TestResults --logger "trx;LogFileName=AdminWebsite-Unit-Tests-TestResults.trx" \
     "/p:CollectCoverage=true" \
@@ -27,7 +25,5 @@ dotnet test AdminWebsite/AdminWebsite.IntegrationTests/AdminWebsite.IntegrationT
     "/p:CoverletOutputFormat=\"opencover,json,cobertura,lcov\""
 
 # Run the Jasmine tests
-# npm install --prefix AdminWebsite/AdminWebsite/ClientApp
-# npm run --prefix AdminWebsite/AdminWebsite/ClientApp test-once-ci
-
-# dotnet sonarscanner end /d:sonar.login="${SONAR_TOKEN}"
+npm install --prefix AdminWebsite/AdminWebsite/ClientApp
+npm run --prefix AdminWebsite/AdminWebsite/ClientApp test-once-ci
