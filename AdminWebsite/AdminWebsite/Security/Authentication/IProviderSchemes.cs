4b1fe77549184f2d907000432a329f22f6562075
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-04-24 18:00:49 +0100

    Register multiple schemes for each IDP

---
+++ AdminWebsite/AdminWebsite/Security/Authentication/IProviderSchemes.cs
@@ -0,0 +1,26 @@
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authentication;
+
+namespace AdminWebsite.Security.Authentication
+{
+    public interface IProviderSchemes
+    {
+        public AuthProvider Provider { get; }
+
+        public string SchemeName { get; }
+
+        public string GetScheme() =>  SchemeName;
+
+        public bool BelongsToScheme(JwtSecurityToken jwtSecurityToken);
+
+        public string[] GetProviderSchemes() => new[] { SchemeName };
+
+        public AuthenticationBuilder AddSchemes(AuthenticationBuilder builder)
+        {
+            builder = AddScheme(builder);
+            return builder;
+        }
+
+        public AuthenticationBuilder AddScheme(AuthenticationBuilder builder);
+    }
+}
