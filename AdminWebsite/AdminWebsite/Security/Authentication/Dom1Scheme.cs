6af79468e3b9ebe141b36be18023014fbb81e288
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-04-25 13:24:08 +0100

    Change audience for DOM1 scheme

--- AdminWebsite/AdminWebsite/Security/Authentication/Dom1Scheme.cs
+++ AdminWebsite/AdminWebsite/Security/Authentication/Dom1Scheme.cs
@@ -1,13 +1,20 @@
 using AdminWebsite.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace AdminWebsite.Security.Authentication
 {
     public class Dom1Scheme : AadSchemeBase
     {
-        public Dom1Scheme(Dom1AdConfiguration dom1AdConfiguration): base(dom1AdConfiguration)
+        public Dom1Scheme(Dom1AdConfiguration dom1AdConfiguration) : base(dom1AdConfiguration)
         {
         }
 
         public override AuthProvider Provider => AuthProvider.Dom1;
+
+        public override void SetJwtBearerOptions(JwtBearerOptions options)
+        {
+            base.SetJwtBearerOptions(options);
+            options.Audience = $"api://{_idpConfiguration.ClientId}";
+        }
     }
 }
