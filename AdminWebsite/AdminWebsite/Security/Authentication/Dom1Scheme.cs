e4440f8ceed3b72b2cadcad7bfe06915de06cbd5
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-04-25 17:32:55 +0100

    Add unique name for name claim type for DOM1 scheme

--- AdminWebsite/AdminWebsite/Security/Authentication/Dom1Scheme.cs
+++ AdminWebsite/AdminWebsite/Security/Authentication/Dom1Scheme.cs
@@ -16,6 +16,7 @@ namespace AdminWebsite.Security.Authentication
             base.SetJwtBearerOptions(options);
             options.Audience = $"api://{_idpConfiguration.ClientId}";
             options.TokenValidationParameters.ValidIssuer = $"https://sts.windows.net/{_idpConfiguration.TenantId}/";
+            options.TokenValidationParameters.NameClaimType= "unique_name";
         }
     }
 }
