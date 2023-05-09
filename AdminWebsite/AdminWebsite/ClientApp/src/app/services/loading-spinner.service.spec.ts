10499c9126131326dbb35fdade6ba2baeb61a1d3
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-05-09 16:25:35 +0100

    fix linting issues

--- AdminWebsite/AdminWebsite/ClientApp/src/app/services/loading-spinner.service.spec.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/services/loading-spinner.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { LoadingSpinnerService } from './loading-spinner.service';
 
 describe('LoadingSpinnerService', () => {
-  let service: LoadingSpinnerService;
+    let service: LoadingSpinnerService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(LoadingSpinnerService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(LoadingSpinnerService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
