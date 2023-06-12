5f45211bc6699808cae0797f4f83e19744b1ccd7
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-06-12 10:06:45 +0100

    delete deprecated suitabilit section

--- AdminWebsite/AdminWebsite/ClientApp/src/app/suitability/services/individual-questionnaire-mapper.ts
+++
@@ -1,9 +0,0 @@
-import { SuitabilityAnswerResponse } from '../../services/clients/api-client';
-import { IndividualQuestionsMapAttributes } from './suitability-answer.mapper';
-import { QuestionnaireMapper } from './questionnaire-mapper';
-
-export class IndividualQuestionnaireMapper extends QuestionnaireMapper {
-    constructor(answers: SuitabilityAnswerResponse[]) {
-        super(answers, new IndividualQuestionsMapAttributes());
-    }
-}
--- AdminWebsite/AdminWebsite/ClientApp/src/app/suitability/services/representative-questionnaire-mapper.ts
+++
@@ -1,9 +0,0 @@
-import { SuitabilityAnswerResponse } from '../../services/clients/api-client';
-import { RepresentativeQuestionsMapAttributes } from './suitability-answer.mapper';
-import { QuestionnaireMapper } from './questionnaire-mapper';
-
-export class RepresentativeQuestionnaireMapper extends QuestionnaireMapper {
-    constructor(answers: SuitabilityAnswerResponse[]) {
-        super(answers, new RepresentativeQuestionsMapAttributes());
-    }
-}
