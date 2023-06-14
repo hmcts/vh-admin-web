8ea6358c2933d0bf09f66f30d15c816037622e95
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-06-14 12:07:28 +0100

    remove unused reference

--- AdminWebsite/AdminWebsite/ClientApp/src/app/manage-team/manage-team.module.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/manage-team/manage-team.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { ManageTeamRoutingModule } from './manage-team-routing.module';
 import { SharedModule } from '../shared/shared.module';
