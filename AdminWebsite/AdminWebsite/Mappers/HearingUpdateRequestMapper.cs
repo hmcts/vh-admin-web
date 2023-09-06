ec31d9f323634745bee60cbbe819f4a82a6cdf63
Oliver Scott <oliver.scott@justice.gov.uk>
2023-09-06 14:10:31 +0100

    VIH-10152 Audio recording required being changed to true on edit for Crime Crown Court or CACD (#1263)

--- AdminWebsite/AdminWebsite/Mappers/HearingUpdateRequestMapper.cs
+++ AdminWebsite/AdminWebsite/Mappers/HearingUpdateRequestMapper.cs
@@ -27,7 +27,7 @@ namespace AdminWebsite.Mappers
                     }
                 },
                 QuestionnaireNotRequired = false,
-                AudioRecordingRequired = editHearingRequest.Participants.Any(x=>x.LinkedParticipants.Any(s=>(int) s.Type == (int) LinkedParticipantType.Interpreter)) || editHearingRequest.AudioRecordingRequired,
+                AudioRecordingRequired = editHearingRequest.AudioRecordingRequired
             };
             return updateHearingRequest;
         }
