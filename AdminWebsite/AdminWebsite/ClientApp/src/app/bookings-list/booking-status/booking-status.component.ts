11e7504a6ec5dc611ea574411e4653a57672a4c1
William Craig <mr.craig1991@gmail.com>
2023-09-19 16:07:43 +0100

    changed booking status divs to be its own component

---
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/bookings-list/booking-status/booking-status.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+import { BookingsDetailsModel } from '../../common/model/bookings-list.model';
+@Component({
+    selector: 'app-booking-status',
+    templateUrl: './booking-status.component.html',
+    styleUrls: ['./booking-status.component.css']
+})
+export class BookingStatusComponent {
+    @Input() bookingDetails: BookingsDetailsModel;
+
+    public get statusMessage(): string {
+        switch (this.bookingDetails.Status) {
+            case 'Created':
+            case 'hasConfirmationWithNoJudge':
+                return 'Confirmed';
+            case 'Cancelled':
+                return 'Cancelled';
+            case 'Failed':
+                return 'Failed';
+            default:
+                return null;
+        }
+    }
+
+    public get hasNoJudge(): boolean {
+        return this.bookingDetails.Status === 'BookedWithoutJudge' || this.bookingDetails.Status === 'ConfirmedWithoutJudge';
+    }
+}
