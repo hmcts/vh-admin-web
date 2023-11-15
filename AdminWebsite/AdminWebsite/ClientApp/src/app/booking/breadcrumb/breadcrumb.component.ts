46fb92ab9f062fe3232b6909e3da813421df5847
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-11-15 17:12:46 +0000

    house keeping on styles deleted unused classes remove bloated imports in place of smaller ones reduced raw build from 12mb to 2.4mb

--- AdminWebsite/AdminWebsite/ClientApp/src/app/booking/breadcrumb/breadcrumb.component.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/booking/breadcrumb/breadcrumb.component.ts
@@ -10,7 +10,7 @@ import { PageUrls } from 'src/app/shared/page-url.constants';
 @Component({
     selector: 'app-breadcrumb',
     templateUrl: './breadcrumb.component.html',
-    styleUrls: ['./breadcrumb.component.css']
+    styleUrls: ['./breadcrumb.component.scss']
 })
 export class BreadcrumbComponent implements OnInit, OnDestroy {
     breadcrumbItems: BreadcrumbItemModel[];
--- AdminWebsite/AdminWebsite/ClientApp/src/polyfills.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/polyfills.ts
@@ -22,25 +22,6 @@ import '@angular/localize/init';
  * BROWSER POLYFILLS
  */
 
-/** IE9, IE10 and IE11 requires all of the following polyfills. **/
-import 'core-js/es/symbol';
-import 'core-js/es/object';
-import 'core-js/es/function';
-import 'core-js/es/parse-int';
-import 'core-js/es/parse-float';
-import 'core-js/es/number';
-import 'core-js/es/math';
-import 'core-js/es/string';
-import 'core-js/es/date';
-import 'core-js/es/array';
-import 'core-js/es/regexp';
-import 'core-js/es/map';
-import 'core-js/es/weak-map';
-import 'core-js/es/set'; // Run `npm install --save classlist.js`.
-
-/** IE10 and IE11 requires the following for the Reflect API. */
-import 'core-js/es/reflect';
-
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
  * user can disable parts of macroTask/DomEvents patch by setting following flags
