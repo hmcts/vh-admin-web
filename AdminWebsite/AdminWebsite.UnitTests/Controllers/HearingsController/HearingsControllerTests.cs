494dceba03cfdb7204e04770a53a1a1b317156e2
Oliver Scott <oliver.scott@justice.gov.uk>
2024-04-05 15:05:55 +0100

    Remove unneeded changes

--- AdminWebsite/AdminWebsite.UnitTests/Controllers/HearingsController/HearingsControllerTests.cs
+++ AdminWebsite/AdminWebsite.UnitTests/Controllers/HearingsController/HearingsControllerTests.cs
@@ -97,9 +97,6 @@ namespace AdminWebsite.UnitTests.Controllers.HearingsController
             {
                 var hearing = Builder<HearingDetailsResponse>.CreateNew().Build();
 
-                var interpreteeId = Guid.NewGuid();
-                var interpreterId = Guid.NewGuid();
-                
                 hearing.Participants = new List<ParticipantResponse>
                 {
                     new()
@@ -110,8 +107,7 @@ namespace AdminWebsite.UnitTests.Controllers.HearingsController
                         ContactEmail = "judge@email.com",
                         Username = "judge@hearings.reform.hmcts.net",
                         HearingRoleName = "Judge",
-                        UserRoleName = "Judge",
-                        LinkedParticipants = new List<LinkedParticipantResponse>()
+                        UserRoleName = "Judge"
                     },
                     new()
                     {
@@ -121,44 +117,7 @@ namespace AdminWebsite.UnitTests.Controllers.HearingsController
                         ContactEmail = "applicant@email.com",
                         Username = "applicant@hearings.reform.hmcts.net",
                         HearingRoleName = "Applicant",
-                        UserRoleName = "Individual",
-                        LinkedParticipants = new List<LinkedParticipantResponse>()
-                    },
-                    new()
-                    {
-                        Id = interpreteeId,
-                        FirstName = "Interpretee",
-                        LastName = "Test",
-                        ContactEmail = "interpretee@email.com",
-                        Username = "interpretee@hearings.reform.hmcts.net",
-                        HearingRoleName = "Applicant",
-                        UserRoleName = "Individual",
-                        LinkedParticipants = new List<LinkedParticipantResponse>
-                        {
-                            new()
-                            {
-                                LinkedId = interpreterId,
-                                Type = LinkedParticipantType.Interpreter
-                            }
-                        }
-                    },
-                    new()
-                    {
-                        Id = interpreterId,
-                        FirstName = "Interpreter",
-                        LastName = "Test",
-                        ContactEmail = "interpreter@email.com",
-                        Username = "interpreter@hearings.reform.hmcts.net",
-                        HearingRoleName = "Interpreter",
-                        UserRoleName = "Individual",
-                        LinkedParticipants = new List<LinkedParticipantResponse>
-                        {
-                            new()
-                            {
-                                LinkedId = interpreteeId,
-                                Type = LinkedParticipantType.Interpreter
-                            }
-                        }
+                        UserRoleName = "Individual"
                     }
                 };
         
@@ -172,17 +131,7 @@ namespace AdminWebsite.UnitTests.Controllers.HearingsController
                     new()
                     {
                         Id = Guid.NewGuid(),
-                        DisplayName = "Endpoint B",
-                        DefenceAdvocateId = hearing.Participants[1].Id
-                    }
-                };
-
-                hearing.Cases = new List<CaseResponse>
-                {
-                    new()
-                    {
-                        Number = "TestCaseNumber",
-                        Name = "TestCaseName"
+                        DisplayName = "Endpoint B"
                     }
                 };
                 
