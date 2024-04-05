8af57a63ccb3fd50defaae32d4419454e4c4b54a
Oliver Scott <oliver.scott@justice.gov.uk>
2024-04-05 15:08:36 +0100

    Remove unneeded changes

--- AdminWebsite/AdminWebsite/Mappers/EditMultiDayHearing/HearingRequestMapper.cs
+++ AdminWebsite/AdminWebsite/Mappers/EditMultiDayHearing/HearingRequestMapper.cs
@@ -1,7 +1,5 @@
 using AdminWebsite.Contracts.Requests;
 using AdminWebsite.Models.EditMultiDayHearing;
-using BookingsApi.Contract.V1.Requests;
-using BookingsApi.Contract.V1.Responses;
 using BookingsApi.Contract.V2.Requests;
 using BookingsApi.Contract.V2.Responses;
 
@@ -9,30 +7,6 @@ namespace AdminWebsite.Mappers.EditMultiDayHearing
 {
     public static class HearingRequestMapper
     {
-        public static HearingRequest MapHearingRequest(HearingDetailsResponse hearing,
-            HearingChanges hearingChanges,
-            EditMultiDayHearingRequest editMultiDayHearingRequest)
-        {
-            var hearingRequest = new HearingRequest
-            {
-                HearingId = hearing.Id,
-                ScheduledDuration = hearingChanges.ScheduledDurationChanged ? 
-                    editMultiDayHearingRequest.ScheduledDuration : hearing.ScheduledDuration,
-                HearingVenueName = hearingChanges.HearingVenueNameChanged ? 
-                    editMultiDayHearingRequest.HearingVenueName : hearing.HearingVenueName,
-                HearingRoomName = hearingChanges.HearingRoomNameChanged ? 
-                    editMultiDayHearingRequest.HearingRoomName : hearing.HearingRoomName,
-                OtherInformation = hearingChanges.OtherInformationChanged ? 
-                    editMultiDayHearingRequest.OtherInformation : hearing.OtherInformation,
-                CaseNumber = hearingChanges.CaseNumberChanged ? 
-                    editMultiDayHearingRequest.CaseNumber : hearing.Cases[0].Number,
-                AudioRecordingRequired = hearingChanges.AudioRecordingRequiredChanged ? 
-                    editMultiDayHearingRequest.AudioRecordingRequired : hearing.AudioRecordingRequired
-            };
-
-            return hearingRequest;
-        }
-        
         public static HearingRequestV2 MapHearingRequestV2(HearingDetailsResponseV2 hearing,
             HearingChanges hearingChanges,
             EditMultiDayHearingRequest editMultiDayHearingRequest)
