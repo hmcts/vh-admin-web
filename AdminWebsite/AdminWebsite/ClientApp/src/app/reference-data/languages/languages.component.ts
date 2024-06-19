a347041314303eaec255fc88e572427cf62124cb
Shaed Parkar <shaed.parkar@slimsolutions.company>
2024-06-19 17:29:27 +0100

    compare existing list with new list

--- AdminWebsite/AdminWebsite/ClientApp/src/app/reference-data/languages/languages.component.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/reference-data/languages/languages.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { parse } from 'papaparse';
 import { RefDataRowItem } from '../models/ref-data.model';
+import { ReferenceDataService } from 'src/app/services/reference-data.service';
+import { AvailableLanguageResponse } from 'src/app/services/clients/api-client';
+
+type LanguageStatus = 'new' | 'removed' | 'modified' | 'unchanged';
+interface LanguageComparisonResult {
+    language: AvailableLanguage;
+    status: LanguageStatus;
+}
 
 @Component({
     selector: 'app-languages',
@@ -10,12 +18,33 @@ import { RefDataRowItem } from '../models/ref-data.model';
 export class LanguagesComponent implements OnInit {
     fileName = '';
     file;
-    signLanguages: RefDataRowItem[];
-    spokenLanguages: RefDataRowItem[];
+    existingDbLanguages: AvailableLanguageResponse[];
+    proposedCsvLanguages: RefDataRowItem[] = [];
+    data: LanguageComparisonResult[] = [];
 
-    constructor() {}
+    constructor(private refDataService: ReferenceDataService) {}
 
-    ngOnInit() {}
+    get unchangedLanguages() {
+        return this.data.filter(x => x.status === 'unchanged');
+    }
+
+    get newLanguages() {
+        return this.data.filter(x => x.status === 'new');
+    }
+
+    get modifiedLanguages() {
+        return this.data.filter(x => x.status === 'modified');
+    }
+
+    get removedLanguages() {
+        return this.data.filter(x => x.status === 'removed');
+    }
+
+    ngOnInit() {
+        this.refDataService.getAvailableInterpreterLanguages().subscribe(data => {
+            this.existingDbLanguages = data;
+        });
+    }
 
     onFileSelected($event: Event) {
         const file = ($event.target as HTMLInputElement).files[0];
@@ -54,10 +83,85 @@ export class LanguagesComponent implements OnInit {
             ParentKey: row.ParentKey,
             Active: row.Active === 'Y' || row.Active === 'y'
         }));
-        this.signLanguages = refDataRows.filter(x => x.CategoryKey === 'SignLanguage');
-        this.spokenLanguages = refDataRows.filter(x => x.CategoryKey === 'InterpreterLanguage');
+        const signLanguages = refDataRows.filter(x => x.CategoryKey === 'SignLanguage');
+        const spokenLanguages = refDataRows.filter(x => x.CategoryKey === 'InterpreterLanguage');
+        this.proposedCsvLanguages = [...signLanguages, ...spokenLanguages];
 
-        console.log('SignLanguages:', this.signLanguages);
-        console.log('SpokenLanguages', this.spokenLanguages);
+        this.data = this.compareLanguages(this.proposedCsvLanguages, this.existingDbLanguages);
+        console.log(this.data);
     }
+
+    compareLanguages(proposedLanguages: RefDataRowItem[], currentLanguages: AvailableLanguageResponse[]): LanguageComparisonResult[] {
+        const comparisonResult: LanguageComparisonResult[] = [];
+
+        const currentLanguageCodes = currentLanguages.map(lang => lang.code);
+
+        // Check for removed, modified, and unchanged languages
+        for (const proposedLanguage of proposedLanguages) {
+            const currentLanguage = currentLanguages.find(lang => lang.code === proposedLanguage.Key);
+
+            if (!currentLanguage) {
+                comparisonResult.push({ language: { code: proposedLanguage.Key, description: proposedLanguage.Value_EN }, status: 'new' });
+            } else if (!proposedLanguage.Active) {
+                comparisonResult.push({
+                    language: { code: proposedLanguage.Key, description: proposedLanguage.Value_EN },
+                    status: 'removed'
+                });
+            } else if (proposedLanguage.Value_EN !== currentLanguage.description) {
+                comparisonResult.push({
+                    language: { code: proposedLanguage.Key, description: proposedLanguage.Value_EN },
+                    status: 'modified'
+                });
+            } else {
+                comparisonResult.push({
+                    language: { code: proposedLanguage.Key, description: proposedLanguage.Value_EN },
+                    status: 'unchanged'
+                });
+            }
+        }
+
+        // Check for removed languages in currentLanguages
+        for (const currentLanguage of currentLanguages) {
+            if (!proposedLanguages.map(lang => lang.Key).includes(currentLanguage.code)) {
+                comparisonResult.push({ language: currentLanguage, status: 'removed' });
+            }
+        }
+
+        return comparisonResult;
+    }
+
+    // compareLanguages(proposedLanguages: RefDataRowItem[], currentLanguages: AvailableLanguageResponse[]): LanguageComparisonResult[] {
+    //     const comparisonResult: LanguageComparisonResult[] = [];
+
+    //     const currentLanguageKeys = proposedLanguages.map(lang => lang.Key);
+    //     const proposedLanguageCodes = currentLanguages.map(lang => lang.code);
+
+    //     // Check for new and modified languages
+    //     for (const currentLanguage of currentLanguages) {
+    //         if (!currentLanguageKeys.includes(currentLanguage.code)) {
+    //             comparisonResult.push({ language: currentLanguage, status: 'removed' });
+    //         } else {
+    //             const currentLanguage = proposedLanguages.find(lang => lang.Key === currentLanguage.code);
+    //             if (currentLanguage.Value_EN !== currentLanguage.description) {
+    //                 comparisonResult.push({ language: currentLanguage, status: 'modified' });
+    //             } else {
+    //                 comparisonResult.push({ language: currentLanguage, status: 'unchanged' });
+    //             }
+    //         }
+    //     }
+
+    //     // Check for new languages
+    //     for (const currentLanguage of proposedLanguages) {
+    //         if (!proposedLanguageCodes.includes(currentLanguage.Key)) {
+    //             comparisonResult.push({
+    //                 language: { code: currentLanguage.Key, description: currentLanguage.Value_EN },
+    //                 status: 'new'
+    //             });
+    //         }
+    //     }
+
+    //     return comparisonResult;
+    // }
 }
+
+export interface AvailableLanguage extends Omit<AvailableLanguageResponse, 'init' | 'toJSON'> {}
