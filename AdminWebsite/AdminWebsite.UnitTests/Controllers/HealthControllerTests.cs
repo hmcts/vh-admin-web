d54bc99753b9eaf78bebbcc7a11462701031590d
William Craig <mr.craig1991@gmail.com>
2024-07-17 10:34:22 +0100

    updated test

--- AdminWebsite/AdminWebsite.UnitTests/Controllers/HealthControllerTests.cs
+++ AdminWebsite/AdminWebsite.UnitTests/Controllers/HealthControllerTests.cs
@@ -1,31 +1,27 @@
 using System.Net;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using AdminWebsite.Configuration;
 using AdminWebsite.Contracts.Responses;
 using AdminWebsite.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Moq.Protected;
 
 namespace AdminWebsite.UnitTests.Controllers;
 
 public class HealthControllerTests
 {
-
-    private Mock<HttpClient> _mockHttpClient;
     private Mock<IOptions<ServiceConfiguration>> _mockServiceConfiguration;
+    private Mock<HttpMessageHandler> _mockMessageHandler;
     
     [SetUp]
     public void Setup()
     {
-        _mockHttpClient = new Mock<HttpClient>();
         _mockServiceConfiguration = new Mock<IOptions<ServiceConfiguration>>();
-        
-        _mockHttpClient.Setup(x => x.GetAsync(It.IsAny<string>())).ReturnsAsync(new HttpResponseMessage());
-        _mockServiceConfiguration.Setup(x => x.Value).Returns(new ServiceConfiguration
-        {
-            BookingsApiUrl = "http://localhost:5000/"
-        });
+        _mockServiceConfiguration.Setup(x => x.Value).Returns(new ServiceConfiguration { BookingsApiUrl = "http://localhost:5000/" });
+        _mockMessageHandler = new Mock<HttpMessageHandler>();
     }
     
     [Test]
@@ -38,9 +34,12 @@ public class HealthControllerTests
             Content = new StringContent("{\"status\":\"Degraded\",\"details\":[{\"key\":\"Database VhBookings\",\"value\":\"Healthy\",\"error\":null},{\"key\":\"Booking Service Bus Queue\",\"value\":\"Degraded\",\"error\":\"status-code: 404.\"}]}")
         };
         
-        _mockHttpClient.Setup(x => x.GetAsync(It.IsAny<string>())).ReturnsAsync(response);
-        
-        var controller = new HealthController(_mockHttpClient.Object, _mockServiceConfiguration.Object);
+        _mockMessageHandler.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(response);
+
+       
+        var controller = new HealthController(new HttpClient(_mockMessageHandler.Object), _mockServiceConfiguration.Object);
         
         // Act
         var result = await controller.GetBookingQueueState();
