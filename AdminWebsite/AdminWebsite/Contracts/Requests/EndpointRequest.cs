ad46ac56720954473ba24b513128b84b96890ae4
marcogagliardi <marco.gagliardi@justice.gov.uk>
2024-08-20 13:28:05 +0100

    VIH-8198 - allow accented letters in display name field in admin web booking form (#1412)
    * changed regex to accept any character in any language and numer
    
    * Update constants.ts
    
    * added -,' to regex
    
    * update package to PR nuget
    
    * update regex
    
    * updated regex for participant request
    
    * added regex to endppoint
    
    * merge
    
    * update new package

--- AdminWebsite/AdminWebsite/Contracts/Requests/EndpointRequest.cs
+++ AdminWebsite/AdminWebsite/Contracts/Requests/EndpointRequest.cs
@@ -5,7 +5,7 @@ namespace AdminWebsite.Contracts.Requests;
 public class EndpointRequest
 {
     [StringLength(255, ErrorMessage = "Display name max length is 255 characters")]
-    [RegularExpression("^([-A-Za-z0-9 \',._])*$")]
+    [RegularExpression(@"^[\p{L}\p{N}\s',._-]+$")]
     public string DisplayName { get; set; }
     public string DefenceAdvocateContactEmail { get; set; }
     public string InterpreterLanguageCode { get; set; }
--- AdminWebsite/AdminWebsite/Models/EditParticipantRequest.cs
+++ AdminWebsite/AdminWebsite/Models/EditParticipantRequest.cs
@@ -53,7 +53,7 @@ namespace AdminWebsite.Models
         ///     Participant Display Name
         /// </summary>
         [StringLength(255, ErrorMessage = "Display name max length is 255 characters")]
-        [RegularExpression("^([-A-Za-z0-9 ',._])*$")]
+        [RegularExpression(@"^[\p{L}\p{N}\s',._-]+$")]
         public string DisplayName { get; set; }
 
         /// <summary>
