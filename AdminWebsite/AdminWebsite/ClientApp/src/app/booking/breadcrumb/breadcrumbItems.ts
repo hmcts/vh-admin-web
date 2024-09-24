25623f851e9c60fc1bec1963c5fe349e6c10b5e2
Shaed Parkar <shaed.parkar@slimsolutions.company>
2024-09-24 14:47:01 +0100

    enable edit screening within 30 mins

--- AdminWebsite/AdminWebsite/ClientApp/src/app/booking/breadcrumb/breadcrumbItems.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/booking/breadcrumb/breadcrumbItems.ts
@@ -7,7 +7,7 @@ export const BreadcrumbItems: BreadcrumbItemModel[] = [
     new BreadcrumbItemModel(4, false, 'Judicial Office Holder(s)', PageUrls.AddJudicialOfficeHolders, false, true),
     new BreadcrumbItemModel(5, false, 'Participants', PageUrls.AddParticipants, false, true),
     new BreadcrumbItemModel(6, false, 'Video access points', PageUrls.Endpoints, false, true),
-    new BreadcrumbItemModel(7, false, 'Screening (Special Measure)', PageUrls.Screening, false, false),
+    new BreadcrumbItemModel(7, false, 'Screening (Special Measure)', PageUrls.Screening, false, true),
     new BreadcrumbItemModel(8, false, 'Other information', PageUrls.OtherInformation, false, false),
     new BreadcrumbItemModel(9, false, 'Summary', PageUrls.Summary, false, true)
 ];
