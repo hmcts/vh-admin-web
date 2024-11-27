a14074c18331079ee37382c96fcf4747d5fe717a
marcogagliardi <marco.gagliardi@justice.gov.uk>
2024-11-27 13:27:29 +0000

    change reverted

--- AdminWebsite/AdminWebsite/Contracts/Responses/AllocationHearingsResponse.cs
+++ AdminWebsite/AdminWebsite/Contracts/Responses/AllocationHearingsResponse.cs
@@ -25,7 +25,7 @@ public class AllocationHearingsResponse
     public string CaseNumber { get; set; }
     
     /// <summary>
-    /// The hearing Service
+    /// The hearing case type
     /// </summary>
     public string CaseType { get; set; }
     
@@ -33,7 +33,7 @@ public class AllocationHearingsResponse
     /// The allocated CSO. Can be one of following:
     /// <list type="bullet">
     ///     <item>"Not Allocated"</item>
-    ///     <item>"Not Required" (if venue is scottish or Service is generic)</item>
+    ///     <item>"Not Required" (if venue is scottish or case type is generic)</item>
     ///     <item>The username of the allocated justice user</item>
     /// </list>
     /// </summary>
