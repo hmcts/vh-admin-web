e09b06f7c8028c20c1d721552aa0aff549d7d964
Oliver Scott <oliver.scott@justice.gov.uk>
2025-02-11 17:10:28 +0000

    More test coverage

--- AdminWebsite/AdminWebsite/ClientApp/src/app/services/reference-data.service.spec.ts
+++ AdminWebsite/AdminWebsite/ClientApp/src/app/services/reference-data.service.spec.ts
@@ -1,5 +1,5 @@
 import { ReferenceDataService } from './reference-data.service';
-import { AvailableLanguageResponse, BHClient, InterprepretationType } from './clients/api-client';
+import { AvailableLanguageResponse, BHClient, CaseTypeResponse, InterprepretationType } from './clients/api-client';
 import { of } from 'rxjs';
 
 describe('ReferenceDataService', () => {
@@ -7,7 +7,7 @@ describe('ReferenceDataService', () => {
     let bhClientSpy: jasmine.SpyObj<BHClient>;
 
     beforeEach(() => {
-        bhClientSpy = jasmine.createSpyObj('BHClient', ['getAvailableLanguages']);
+        bhClientSpy = jasmine.createSpyObj('BHClient', ['getAvailableLanguages', 'getCaseTypes']);
         service = new ReferenceDataService(bhClientSpy);
     });
 
@@ -42,6 +42,22 @@ describe('ReferenceDataService', () => {
             service.getAvailableInterpreterLanguages().subscribe(languages => {
                 expect(languages).toEqual(expectedLanguages);
                 expect(bhClientSpy.getAvailableLanguages).toHaveBeenCalledTimes(1);
+            });
+        });
+    });
+
+    describe('getCaseTypes', () => {
+        it('should return case types', () => {
+            const expectedCaseTypes = [
+                new CaseTypeResponse({ id: 1, name: 'Civil', service_id: 'CI', is_audio_recording_allowed: true }),
+                new CaseTypeResponse({ id: 2, name: 'Family', service_id: 'FA', is_audio_recording_allowed: true }),
+                new CaseTypeResponse({ id: 3, name: 'Criminal', service_id: 'CR', is_audio_recording_allowed: true })
+            ];
+            bhClientSpy.getCaseTypes.and.returnValue(of(expectedCaseTypes));
+
+            service.getCaseTypes().subscribe(caseTypes => {
+                expect(caseTypes).toEqual(expectedCaseTypes);
+                expect(bhClientSpy.getCaseTypes).toHaveBeenCalled();
             });
         });
     });
