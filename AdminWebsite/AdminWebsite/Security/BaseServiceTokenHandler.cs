21057255094e471975cf5a8841cf1fe6bc66a268
William Craig <mr.craig1991@gmail.com>
2025-03-10 09:47:27 +0000

    Tidy up of baseServiceToken Handler

--- AdminWebsite/AdminWebsite/Security/BaseServiceTokenHandler.cs
+++ AdminWebsite/AdminWebsite/Security/BaseServiceTokenHandler.cs
@@ -1,55 +1,42 @@
-﻿using System.Collections.Generic;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using AdminWebsite.Configuration;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Options;
 
-namespace AdminWebsite.Security
+namespace AdminWebsite.Security;
+
+public abstract class BaseServiceTokenHandler(
+    IOptions<AzureAdConfiguration> azureAdConfiguration,
+    IOptions<ServiceConfiguration> serviceConfiguration,
+    IMemoryCache memoryCache,
+    ITokenProvider tokenProvider)
+    : DelegatingHandler
 {
-    public abstract class BaseServiceTokenHandler : DelegatingHandler
+    const string AUTHORIZATION = "Authorization";
+    const string BEARER = "Bearer";
+    const int MINUTES = -1;
+
+    private readonly AzureAdConfiguration _azureAdConfiguration = azureAdConfiguration.Value;
+    protected readonly ServiceConfiguration ServiceConfiguration = serviceConfiguration.Value;
+
+    protected abstract string TokenCacheKey { get; }
+    protected abstract string ClientResource { get; }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        private readonly AzureAdConfiguration _azureAdConfiguration;
-        private readonly IMemoryCache _memoryCache;
-        private readonly ITokenProvider _tokenProvider;
-        protected readonly ServiceConfiguration ServiceConfiguration;
-
-        protected BaseServiceTokenHandler(IOptions<AzureAdConfiguration> azureAdConfiguration,
-            IOptions<ServiceConfiguration> serviceConfiguration,
-            IMemoryCache memoryCache,
-            ITokenProvider tokenProvider)
+        var token = memoryCache.Get<string>(TokenCacheKey);
+        if (string.IsNullOrEmpty(token))
         {
-            _azureAdConfiguration = azureAdConfiguration.Value;
-            ServiceConfiguration = serviceConfiguration.Value;
-            _memoryCache = memoryCache;
-            _tokenProvider = tokenProvider;
+            var authenticationResult = await tokenProvider.GetAuthorisationResult(_azureAdConfiguration.ClientId, _azureAdConfiguration.ClientSecret, ClientResource);
+            token = authenticationResult.AccessToken;
+            var tokenExpireDateTime = authenticationResult.ExpiresOn.DateTime.AddMinutes(MINUTES);
+            memoryCache.Set(TokenCacheKey, token, tokenExpireDateTime);
         }
 
-        protected abstract string TokenCacheKey { get; }
-        protected abstract string ClientResource { get; }
+        request.Headers.Add(AUTHORIZATION, $"{BEARER} {token}");
 
-        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-        {
-            const string AUTHORIZATION = "Authorization";
-            const string BEARER = "Bearer";
-            const int MINUTES = -1;
-
-            var properties = new Dictionary<string, string>();
-
-            var token = _memoryCache.Get<string>(TokenCacheKey);
-            if (string.IsNullOrEmpty(token))
-            {
-                var authenticationResult = await _tokenProvider.GetAuthorisationResult(_azureAdConfiguration.ClientId, _azureAdConfiguration.ClientSecret, ClientResource);
-                token = authenticationResult.AccessToken;
-                var tokenExpireDateTime = authenticationResult.ExpiresOn.DateTime.AddMinutes(MINUTES);
-                _memoryCache.Set(TokenCacheKey, token, tokenExpireDateTime);
-            }
-
-            request.Headers.Add(AUTHORIZATION, $"{BEARER} {token}");
-
-            return await base.SendAsync(request, cancellationToken);
-
-        }
+        return await base.SendAsync(request, cancellationToken);
     }
 }
